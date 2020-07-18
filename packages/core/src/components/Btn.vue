9b3ac23f103c78f9c26f732422a481977d6474a5
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2020-07-19 00:07:13 +0200

    Implemented Btn component

---
+++ packages/core/src/components/Btn.vue
@@ -0,0 +1,93 @@
+<template>
+  <component
+    :is="component"
+    :type="type"
+    :aria-disabled="disabled || ariaDisabled"
+    :disabled="disabled"
+    :class="[styles.button, styles.modifiers[variant], {
+      [styles.modifiers.block]: block,
+      [styles.modifiers.disabled]: disabled,
+      [styles.modifiers.ariaDisabled]: ariaDisabled,
+      [styles.modifiers.active]: active,
+      [styles.modifiers.inline]: inline && variant === 'link',
+      [styles.modifiers.small]: small,
+    }]"
+    :tabindex="tabIdx"
+  >
+    <span v-if="variant !== 'plain' && $slots.icon && iconPosition === 'left'" :class="[styles.buttonIcon, styles.modifiers.start]">
+      <slot name="icon" />
+    </span>
+    <slot />
+    <span v-if="variant !== 'plain' && $slots.icon && iconPosition === 'right'" :class="[styles.buttonIcon, styles.modifiers.end]">
+      <slot name="icon" />
+    </span>
+  </component>
+</template>
+
+<script>
+import _styles from '@patternfly/react-styles/css/components/Page/page';
+
+// commonjs bug
+let styles = _styles.default;
+
+export default {
+  name: 'Btn',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+
+    variant: {
+      type: String,
+      default: 'primary',
+      validate: v => ['primary', 'secondary', 'tertiary', 'danger', 'link', 'plain', 'control'].includes(v),
+    },
+
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validate: v => ['left', 'right'].includes(v),
+    },
+
+    component: {
+      type: String,
+      default: 'button',
+    },
+
+    active: Boolean,
+    block: Boolean,
+    disabled: Boolean,
+
+    /** @beta Adds disabled styling and communicates that the button is disabled using the aria-disabled html attribute */
+    ariaDisabled: Boolean,
+
+    inline: Boolean,
+    small: Boolean,
+
+    tabindex: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      styles,
+    };
+  },
+
+  computed: {
+    tabIdx() {
+      if (this.tabindex) {
+        return this.tabindex;
+      }
+      if ((this.disabled && this.component === 'button') || this.ariaDisabled) {
+        return null;
+      }
+      return -1;
+    },
+  },
+};
+</script>
--- packages/core/src/components/index.js
+++ packages/core/src/components/index.js
@@ -1,1 +1,1 @@
-export default {};
+export { default as Btn } from './Btn.vue';
