56036be30d84fcbea23c9a44a98ba3734d0ed496
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2020-07-18 20:36:16 +0200

    Implemented Grid layout

--- packages/core/src/layouts/index.js
+++ packages/core/src/layouts/index.js
@@ -3,3 +3,5 @@ export { default as Flex } from './Flex/Flex.vue';
 export { default as FlexItem } from './Flex/FlexItem.vue';
 export { default as Gallery } from './Gallery/Gallery.vue';
 export { default as GalleryItem } from './Gallery/GalleryItem';
+export { default as Grid } from './Grid/Grid.vue';
+export { default as GridItem } from './Grid/GridItem.vue';
