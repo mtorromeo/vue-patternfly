0d229a2c71347155efd62399524727f74b9a79cc
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2020-07-20 21:32:35 +0200

    Added index to the components/layout groups

---
+++ packages/core/src/components/Nav/index.js
@@ -0,0 +1,6 @@
+export {default as Nav} from './Nav.vue';
+export {default as NavList} from './NavList.vue';
+export {default as NavGroup} from './NavGroup';
+export {default as NavItem} from './NavItem.vue';
+export {default as NavItemSeparator} from './NavItemSeparator';
+export {default as NavExpandable} from './NavExpandable.vue';
---
+++ packages/core/src/components/Page/index.js
@@ -0,0 +1,7 @@
+export {default as Page} from './Page.vue';
+export {default as PageHeader} from './PageHeader.vue';
+export {default as PageSidebar} from './PageSidebar.vue';
+export {default as PageSection} from './PageSection';
+export {default as PageHeaderTools} from './PageHeaderTools';
+export {default as PageHeaderToolsGroup} from './PageHeaderToolsGroup';
+export {default as PageHeaderToolsItem} from './PageHeaderToolsItem';
