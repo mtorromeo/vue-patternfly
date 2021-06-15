bcf008ea2d3d3f979340e6b405cdddf99d8e8d47
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-06-15 18:11:31 +0200

    Implemented Avatar component

---
+++ packages/core/src/components/Avatar.vue
@@ -0,0 +1,30 @@
+<script>
+import {h, mergeProps} from 'vue';
+import styles from '@patternfly/react-styles/css/components/Avatar/avatar';
+
+export default {
+  name: 'PfAvatar',
+
+  inheritAttrs: true,
+
+  props: {
+    /** Attribute that specifies the URL of the image for the Avatar. */
+    src: {
+      type: String,
+      default: "",
+    },
+
+    /** Attribute that specifies the alternate text of the image for the Avatar. */
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+
+  render() {
+    return h('img', mergeProps({
+      class: styles.avatar,
+    }, this.$props));
+  },
+};
+</script>
---
+++ packages/docs/stories/Avatar.stories.js
@@ -0,0 +1,21 @@
+import PfAvatar from "../../core/src/components/Avatar.vue";
+import {argTypesFromProps} from '../utils';
+import avatarImg from '../examples/avatar.svg';
+
+export default {
+  title: "Components/Avatar",
+  component: PfAvatar,
+  argTypes: argTypesFromProps(PfAvatar.props),
+};
+
+export const Default = (args) => ({
+  components: { PfAvatar },
+  setup() {
+    return { args };
+  },
+  template: '<pf-avatar v-bind="args" />',
+});
+Default.args = {
+  src: avatarImg,
+  alt: "avatar",
+};
