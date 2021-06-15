b2d09110afab1cff25c4b0b9b3c612929bfae600
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-06-15 20:09:02 +0200

    [docs] Badge

---
+++ packages/docs/stories/components/Badge.stories.js
@@ -0,0 +1,21 @@
+import PfBadge from '../../../core/src/components/Badge';
+import {argTypesFromProps} from '../../utils';
+
+export default {
+  title: "Components/Badge",
+  component: PfBadge,
+  argTypes: argTypesFromProps(PfBadge.props),
+};
+
+const Template = ({label, ...args}) => ({
+  components: { PfBadge },
+  setup() {
+    return { args, label };
+  },
+  template: '<pf-badge v-bind="args">{{ label }}</pf-badge>',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "24",
+};
