ff17296bdbb903e1709e5f2a1d89b57705bb3e4e
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-06-15 20:56:42 +0200

    [docs] Nav

---
+++ packages/docs/stories/components/Nav.stories.js
@@ -0,0 +1,125 @@
+import {
+  PfNav,
+  PfNavList,
+  PfNavItem,
+  PfNavGroup,
+} from "../../../core/src/components/Nav";
+import { argTypesFromProps } from "../../utils";
+import { ref } from "vue";
+
+const argTypes = argTypesFromProps(PfNav.props);
+
+export default {
+  title: "Components/Nav",
+  component: PfNav,
+  subcomponents: { PfNavList, PfNavItem, PfNavGroup },
+  argTypes: {
+    ...argTypes,
+    theme: {
+      ...argTypes.theme,
+      control: {
+        type: "select",
+        options: ["dark", "light"],
+      },
+    },
+    variant: {
+      ...argTypes.variant,
+      control: {
+        type: "select",
+        options: ["default", "horizontal", "tertiary"],
+      },
+    },
+  },
+};
+
+export const Default = ({ ...args }) => ({
+  components: { PfNav, PfNavList, PfNavItem },
+  setup() {
+    const activeLink = ref("link1");
+    return { args, activeLink };
+  },
+  template: `
+    <div :style="{'background-color': args.theme == 'light' || args.variant === 'tertiary' ? null : 'var(--pf-global--BackgroundColor--dark-300)'}">
+      <pf-nav v-bind="args">
+        <pf-nav-list>
+          <pf-nav-item item-id="link1" :active="activeLink == 'link1'" @select="selectLink">
+            Link 1
+          </pf-nav-item>
+          <pf-nav-item item-id="link2" :active="activeLink == 'link2'" @select="selectLink">
+            Link 2
+          </pf-nav-item>
+          <pf-nav-item item-id="link3" :active="activeLink == 'link3'" @select="selectLink">
+            Link 3
+          </pf-nav-item>
+          <pf-nav-item item-id="link4" :active="activeLink == 'link4'" @select="selectLink">
+            Link 4
+          </pf-nav-item>
+          <pf-nav-item item-id="link5" :active="activeLink == 'link5'" @select="selectLink">
+            Link 5
+          </pf-nav-item>
+        </pf-nav-list>
+      </pf-nav>
+    </div>
+  `,
+  methods: {
+    selectLink(e, groupid, itemid) {
+      this.activeLink = itemid;
+    },
+  },
+});
+
+export const Grouped = ({ ...args }) => ({
+  components: { PfNav, PfNavList, PfNavItem, PfNavGroup },
+  setup() {
+    const activeLink = ref("link1");
+    return { args, activeLink };
+  },
+  template: `
+    <div :style="{'background-color': args.theme == 'light' || args.variant === 'tertiary' ? null : 'var(--pf-global--BackgroundColor--dark-300)'}">
+      <pf-nav v-bind="args">
+        <pf-nav-list>
+          <pf-nav-group title="Section title 1">
+            <pf-nav-item item-id="link1" :active="activeLink == 'link1'" @select="selectLink">
+              Link 1
+            </pf-nav-item>
+            <pf-nav-item item-id="link2" :active="activeLink == 'link2'" @select="selectLink">
+              Link 2
+            </pf-nav-item>
+            <pf-nav-item item-id="link3" :active="activeLink == 'link3'" @select="selectLink">
+              Link 3
+            </pf-nav-item>
+          </pf-nav-group>
+          <pf-nav-group title="Section title 2">
+            <pf-nav-item item-id="link4" :active="activeLink == 'link4'" @select="selectLink">
+              Link 4
+            </pf-nav-item>
+            <pf-nav-item item-id="link5" :active="activeLink == 'link5'" @select="selectLink">
+              Link 5
+            </pf-nav-item>
+            <pf-nav-item item-id="link6" :active="activeLink == 'link6'" @select="selectLink">
+              Link 6
+            </pf-nav-item>
+          </pf-nav-group>
+        </pf-nav-list>
+      </pf-nav>
+    </div>
+  `,
+  methods: {
+    selectLink(e, groupid, itemid) {
+      this.activeLink = itemid;
+    },
+  },
+});
+
+// export const SimpleCategory = Template.bind({});
+// SimpleCategory.args = {
+//   ...chips,
+//   category: "Category One",
+// };
+
+// export const CategoryRemovable = Template.bind({});
+// CategoryRemovable.args = {
+//   ...chips,
+//   category: "Category has a very long name",
+//   closable: true,
+// };
