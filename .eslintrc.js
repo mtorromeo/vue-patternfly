3df6b1619dfbb14cbdd26ebee3450fda9c6d2edc
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-06-20 11:53:58 +0200

    Removed lint not respected by vscode formatters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-  ],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/max-attributes-per-line': 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/html-closing-bracket-newline": "off",
+    "comma-dangle": ["error", "always-multiline"],
   },
 };
