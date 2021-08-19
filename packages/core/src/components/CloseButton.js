28577fe8b67929aab0bd25d2647b1b3cc8a50c7e
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-08-19 19:56:29 +0200

    fix(close-button): Better aria-label

--- packages/core/src/components/CloseButton.js
+++ packages/core/src/components/CloseButton.js
@@ -5,7 +5,7 @@ import TimesIcon from '@vue-patternfly/icons/dist/esm/icons/times-icon';
 const PfCloseButton = (props, { attrs }) => h(PfButton, mergeProps({
   type: 'button',
   variant: 'plain',
-  'aria-label': 'label-close-button',
+  'aria-label': 'Close',
 }, attrs), {
   default: () => h(TimesIcon),
 });
