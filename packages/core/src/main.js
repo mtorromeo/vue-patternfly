44f900b172c2c2ffd3cbb8f10d5f3ed9ff3e0b0b
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-08-19 11:29:33 +0200

    fix: Removed usage of Vue.use

--- packages/core/src/main.js
+++ packages/core/src/main.js
@@ -19,17 +19,6 @@ const plugin = {
   install,
 };
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
 // To allow use as module (npm/webpack/etc.) export components
 export * from './layouts';
 export * from './components';
