513d478510a86ab31fa93e486aab788e808d2307
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-08-24 18:46:52 +0200

    feat(Accordion): Added support for bordered modifiers, display large and mutilple body content

--- packages/core/src/components/Accordion/Accordion.vue
+++ packages/core/src/components/Accordion/Accordion.vue
@@ -1,5 +1,5 @@
 <script>
-import {h, mergeProps} from 'vue';
+import { h } from 'vue';
 import styles from '@patternfly/react-styles/css/components/Accordion/accordion';
 
 export default {
@@ -12,21 +12,30 @@ export default {
   },
 
   props: {
+    /** Heading level to use */
     level: {
       type: Number,
       default: 3,
       validator: v => v >= 1 && v <= 6,
     },
 
+    /** Flag to indicate whether use definition list or div */
     dl: Boolean,
+
+    /** Flag to indicate the accordion had a border */
+    bordered: Boolean,
+
+    /** Large display size variant. */
+    large: Boolean,
   },
 
   render() {
-    return h(this.dl ? 'dl' : 'div', mergeProps({
-      class: styles.accordion,
-    }, this.$attrs), {
-      default: () => this.$slots.default && this.$slots.default(),
-    });
+    return h(this.dl ? 'dl' : 'div', {
+      class: [styles.accordion, {
+        [styles.modifiers.bordered]: this.bordered,
+        [styles.modifiers.displayLg]: this.large,
+      }],
+    }, this.$slots);
   },
 };
 </script>
