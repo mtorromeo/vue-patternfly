7747a708dd4f8d3bd9282431a3236af49fb8137f
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-08-24 17:01:47 +0200

    feat: FormSection

---
+++ packages/core/src/components/Form/FormSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section :class="styles.formSection">
+    <component :is="titleElement" v-if="title" :class="styles.formSectionTitle">
+      {{ title }}
+    </component>
+    <slot />
+  </section>
+</template>
+
+<script>
+import styles from '@patternfly/react-styles/css/components/Form/form';
+
+export default {
+  name: 'PfFormSection',
+
+  props: {
+    /** Title for the section */
+    title: {
+      type: String,
+      default: '',
+    },
+
+    /** Header description */
+    titleElement: {
+      type: String,
+      default: 'div',
+      validator: v => ['div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(v),
+    },
+  },
+
+  data() {
+    return {
+      styles,
+    };
+  },
+};
+</script>
--- packages/core/src/components/Form/index.js
+++ packages/core/src/components/Form/index.js
@@ -3,3 +3,4 @@ export { default as PfFormAlert } from './FormAlert.js';
 export { default as PfFormGroup } from './FormGroup.vue';
 export { default as PfFormFieldGroup } from './FormFieldGroup.vue';
 export { default as PfFormFieldGroupHeader } from './FormFieldGroupHeader.vue';
+export { default as PfFormSection } from './FormSection.vue';
