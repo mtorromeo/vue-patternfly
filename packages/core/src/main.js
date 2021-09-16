bc3e424d9d40cc56d0a26764894193de9557a1e4
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-09-16 17:47:40 +0200

    chore: cleanup

--- packages/core/src/main.js
+++ packages/core/src/main.js
@@ -1,26 +1,18 @@
 import * as layouts from './layouts';
 import * as components from './components';
+export * from './layouts';
+export * from './components';
 
 const all = { ...layouts, ...components };
 
-// Declare install function executed by Vue.use()
 export function install(app) {
-  if (install.installed) {
-    return;
-  }
-  install.installed = true;
   for (const [name, component] of Object.entries(all)) {
     app.component(component.name || name, component);
   }
 }
 
-// Create module definition for Vue.use()
 const plugin = {
   install,
 };
-
-// To allow use as module (npm/webpack/etc.) export components
-export * from './layouts';
-export * from './components';
 
 export default plugin;
