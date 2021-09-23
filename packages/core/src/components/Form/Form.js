2a38f89f30cf611d7505e05a3a5621e917ee3702
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-09-23 19:38:12 +0200

    feat(Form): allow setting alternative component

--- packages/core/src/components/Form/Form.js
+++ packages/core/src/components/Form/Form.js
@@ -1,17 +1,22 @@
 import styles from '@patternfly/react-styles/css/components/Form/form';
 
-import { h } from 'vue';
+import { h, resolveDynamicComponent } from 'vue';
 
 export default {
   name: 'PfForm',
 
   props: {
+    component: {
+      type: String,
+      default: 'form',
+    },
+
     horizontal: Boolean,
     widthLimited: Boolean,
   },
 
   render() {
-    return h('form', {
+    return h(resolveDynamicComponent(this.component), {
       class: [styles.form, {
         [styles.modifiers.horizontal]: this.horizontal,
         [styles.modifiers.limitWidth]: this.widthLimited,
