7b128b3f925f73184cae839e5bcea058905bb851
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-11-10 17:27:19 +0100

    docs: migrated storybook builder to vite

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,28 +1,33 @@
-const path = require('path');
-
 module.exports = {
   stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
   ],
-  webpackFinal(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, '..');
-    config.resolve.alias['@vue-patternfly/core'] = path.resolve(__dirname, '../../core');
-
-    // https://github.com/vueuse/vueuse/issues/718
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto',
-    });
-
-    return config;
+  framework: "@storybook/vue3",
+  core: {
+    builder: "storybook-builder-vite"
   },
   features: {
     previewCsfV3: true,
   },
-};
+
+  async viteFinal(config, { configType }) {
+    if (configType === "DEVELOPMENT") {
+      // customize the Vite config here
+      config.server.port = 6001;
+      config.server.https = false;
+      config.server.host = true;
+      config.server.hmr = {
+        port: 10443,
+        protocol: "ws",
+      };
+    }
+
+    // return the customized config
+    return config;
+  },
+}
