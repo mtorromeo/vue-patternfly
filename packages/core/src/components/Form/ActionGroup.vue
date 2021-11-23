38b0e241f9e18aeda86ce5bfc6bf253cad84c212
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2021-11-23 15:50:49 +0100

    feat: ActionGroup component

---
+++ packages/core/src/components/Form/ActionGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="[styles.formGroup, styles.modifiers.action]">
+    <div :class="styles.formGroupControl">
+      <div :class="styles.formActions">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import styles from '@patternfly/react-styles/css/components/Form/form';
+</script>
+
+<script>
+export default {
+  name: 'PfActionGroup',
+};
+</script>
--- packages/core/src/components/Form/index.js
+++ packages/core/src/components/Form/index.js
@@ -4,3 +4,4 @@ export { default as PfFormGroup } from './FormGroup.vue';
 export { default as PfFormFieldGroup } from './FormFieldGroup.vue';
 export { default as PfFormFieldGroupHeader } from './FormFieldGroupHeader.vue';
 export { default as PfFormSection } from './FormSection.vue';
+export { default as PfActionGroup } from './ActionGroup.vue';
