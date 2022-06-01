f6fb7601edcc18f995618f5b90074140a50dec28
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2022-06-01 09:37:24 +0200

    chore: fixed eslint false-positives with script-setup components

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript/recommended',
+    'plugin:vue/vue3-recommended',
   ],
   parserOptions: {
     parser: "@typescript-eslint/parser",
