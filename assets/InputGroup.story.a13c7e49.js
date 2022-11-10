206a74b500dc4680185ffe8cac3708da98e64690
mtorromeo <massimiliano.torromeo@gmail.com>
2022-11-10 19:52:53 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly4@53210337b4b0b312ad8e8bbd91a593b21e1f6752 🚀

---
+++ assets/InputGroup.story.a13c7e49.js
@@ -0,0 +1,14 @@
+import{c as f}from"./createIcon.79e32285.js";import{M as r}from"./magnifying-glass-icon.ecf9dbe3.js";import{e as l,a as p,o as _,c as g,w as n,b as t,g as m,u as i}from"./index.e4110fe9.js";const x={name:"AtIcon",height:512,width:512,svgPath:"M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192c0-35.3-28.7-64-64-64s-64 28.7-64 64s28.7 64 64 64s64-28.7 64-64z",yOffset:0,xOffset:0},d=f(x),y=d,G=l({__name:"InputGroup.story",setup(h){return(I,C)=>{const a=p("component-title"),e=p("pf-text-input"),o=p("pf-input-group-text"),u=p("pf-input-group"),c=p("story-canvas"),s=p("doc-page");return _(),g(s,{title:"Input Group"},{default:n(()=>[t(a,{name:"pf-input-group"}),t(c,{title:"Default",source:`<pf-input-group>
+  <pf-text-input type="email" ></pf-text-input>
+  <pf-input-group-text>@example.com</pf-input-group-text>
+</pf-input-group>`},{default:n(()=>[t(u,null,{default:n(()=>[t(e,{type:"email"}),t(o,null,{default:n(()=>[m("@example.com")]),_:1})]),_:1})]),_:1}),t(c,{title:"With pre icon",source:`<pf-input-group>
+  <pf-input-group-text>
+    <at-icon></at-icon>
+  </pf-input-group-text>
+  <pf-text-input type="email" ></pf-text-input>
+</pf-input-group>`},{default:n(()=>[t(u,null,{default:n(()=>[t(o,null,{default:n(()=>[t(i(y))]),_:1}),t(e,{type:"email"})]),_:1})]),_:1}),t(c,{title:"With post icon",source:`<pf-input-group>
+  <pf-text-input type="search" ></pf-text-input>
+  <pf-input-group-text>
+    <magnifying-glass-icon></magnifying-glass-icon>
+  </pf-input-group-text>
+</pf-input-group>`},{default:n(()=>[t(u,null,{default:n(()=>[t(e,{type:"search"}),t(o,null,{default:n(()=>[t(i(r))]),_:1})]),_:1})]),_:1})]),_:1})}}});export{G as default};
---
+++ assets/TextInput.story.a088f895.js
@@ -0,0 +1,51 @@
+import{e as m,r as p,a as u,o as x,c as v,w as t,b as e,d as i}from"./index.e4110fe9.js";const f=i("br",null,null,-1),b=i("br",null,null,-1),_=i("br",null,null,-1),h=i("br",null,null,-1),g=i("br",null,null,-1),q=i("br",null,null,-1),z=m({__name:"TextInput.story",setup(w){const r=p(""),d=p("");return(C,n)=>{const s=u("component-title"),a=u("pf-text-input"),l=u("story-canvas"),c=u("doc-page");return x(),v(c,{title:"Text Input"},{default:t(()=>[e(s,{name:"pf-text-input"}),e(l,{title:"Basic",source:'<pf-text-input v-model="text1" aria-label="text input example" ></pf-text-input>'},{default:t(()=>[e(a,{modelValue:r.value,"onUpdate:modelValue":n[0]||(n[0]=o=>r.value=o),"aria-label":"text input example"},null,8,["modelValue"])]),_:1}),e(l,{title:"Disabled",source:`<pf-text-input
+disabled
+value="disabled text input example"
+aria-label="disabled text input example" />`},{default:t(()=>[e(a,{disabled:"",value:"disabled text input example","aria-label":"disabled text input example"})]),_:1}),e(l,{title:"Read only",source:`<pf-text-input
+readonly
+value="read only text input example"
+aria-label="read only text input example" />`},{default:t(()=>[e(a,{readonly:"",value:"read only text input example","aria-label":"read only text input example"})]),_:1}),e(l,{title:"Validation",source:`<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (does not validate)"
+  aria-label="does not validate"
+  :auto-validate="false" />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on enter/submit)"
+  aria-label="validates on enter/submit"
+  auto-validate />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on blur)"
+  aria-label="validates on blur"
+  auto-validate="blur" />
+<br>
+<pf-text-input
+  v-model="text2"
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on change)"
+  aria-label="validates on change"
+  auto-validate="change" />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on input)"
+  aria-label="validates on input"
+  auto-validate="input" />`},{default:t(()=>[e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (does not validate)","aria-label":"does not validate","auto-validate":!1}),f,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on enter/submit)","aria-label":"validates on enter/submit","auto-validate":""}),b,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on blur)","aria-label":"validates on blur","auto-validate":"blur"}),_,e(a,{modelValue:d.value,"onUpdate:modelValue":n[1]||(n[1]=o=>d.value=o),required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on change)","aria-label":"validates on change","auto-validate":"change"},null,8,["modelValue"]),h,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on input)","aria-label":"validates on input","auto-validate":"input"})]),_:1}),e(l,{title:"Truncated on left",source:'<pf-text-input left-trucated model-value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." ></pf-text-input>'},{default:t(()=>[e(a,{"left-trucated":"","model-value":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."})]),_:1}),e(l,{title:"Select all",source:`<pf-text-input
+value="select all on click"
+aria-label="select-all"
+@focus="($event.currentTarget as HTMLInputElement).select()" />`},{default:t(()=>[e(a,{value:"select all on click","aria-label":"select-all",onFocus:n[2]||(n[2]=o=>o.currentTarget.select())})]),_:1}),e(l,{title:"Icon variants",source:`<pf-text-input aria-label="text input example" icon-variant="calendar" ></pf-text-input>
+<br>
+<pf-text-input aria-label="text input example" icon-variant="clock" ></pf-text-input>
+<br>
+<pf-text-input
+  aria-label="text input example"
+  icon-dimensions="24px 24px"
+  icon-url="data:image/svg+xml;charset=utf8,%3Csvg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 512 512&quot;%3E%3Cpath fill=&quot;%23a18fff&quot; d=&quot;M158.87.15c-16.16-1.52-31.2 8.42-35.33 24.12l-14.81 56.27c187.62 5.49 314.54 130.61 322.48 317l56.94-15.78c15.72-4.36 25.49-19.68 23.62-35.9C490.89 165.08 340.78 17.32 158.87.15zm-58.47 112L.55 491.64a16.21 16.21 0 0 0 20 19.75l379-105.1c-4.27-174.89-123.08-292.14-299.15-294.1zM128 416a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm48-152a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm104 104a32 32 0 1 1 32-32 32 32 0 0 1-32 32z&quot;/%3E%3C/svg%3E" />`},{default:t(()=>[e(a,{"aria-label":"text input example","icon-variant":"calendar"}),g,e(a,{"aria-label":"text input example","icon-variant":"clock"}),q,e(a,{"aria-label":"text input example","icon-dimensions":"24px 24px","icon-url":'data:image/svg+xml;charset=utf8,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"%3E%3Cpath fill="%23a18fff" d="M158.87.15c-16.16-1.52-31.2 8.42-35.33 24.12l-14.81 56.27c187.62 5.49 314.54 130.61 322.48 317l56.94-15.78c15.72-4.36 25.49-19.68 23.62-35.9C490.89 165.08 340.78 17.32 158.87.15zm-58.47 112L.55 491.64a16.21 16.21 0 0 0 20 19.75l379-105.1c-4.27-174.89-123.08-292.14-299.15-294.1zM128 416a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm48-152a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm104 104a32 32 0 1 1 32-32 32 32 0 0 1-32 32z"/%3E%3C/svg%3E'})]),_:1})]),_:1})}}});export{z as default};
