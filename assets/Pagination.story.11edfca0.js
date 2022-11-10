f6c2b70d2734111ebf7c63816166dd9ac2fb4fca
mtorromeo <massimiliano.torromeo@gmail.com>
2022-11-10 11:51:53 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly4@d00b7aad5533c06185eafc25448bc35760b79b7e 🚀

---
+++ assets/Pagination.story.11edfca0.js
@@ -0,0 +1,4 @@
+import{e as _,r as s,a as n,c as m,w as r,o as u,b as o}from"./index.9029d213.js";const x=_({__name:"Pagination.story",setup(d){const t=s(1),p=s(10);return(f,e)=>{const c=n("component-title"),g=n("pf-pagination"),i=n("story-canvas"),l=n("doc-page");return u(),m(l,{title:"Pagination"},{default:r(()=>[o(c,{name:"pf-pagination"}),o(i,{title:"Example",source:`<pf-pagination
+v-model:page="page"
+v-model:per-page="perPage"
+:count="333" />`},{default:r(()=>[o(g,{page:t.value,"onUpdate:page":e[0]||(e[0]=a=>t.value=a),"per-page":p.value,"onUpdate:per-page":e[1]||(e[1]=a=>p.value=a),count:333},null,8,["page","per-page"])]),_:1})]),_:1})}}});export{x as default};
