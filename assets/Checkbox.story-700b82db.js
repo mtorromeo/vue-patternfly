44fcd2710531f6ec74adfeccbe624b981dd3f4e3
mtorromeo <massimiliano.torromeo@gmail.com>
2023-02-16 14:36:44 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly4@f58f7ab19add94bb5c3d9dda8c046f1129b6400c 🚀

---
+++ assets/Checkbox.story-700b82db.js
@@ -0,0 +1,8 @@
+import{_ as s,a as t,o as r,c as d,w as o,b as e}from"./index-f48eb9bb.js";const l={};function h(b,p){const a=t("component-title"),c=t("pf-checkbox"),i=t("story-canvas"),n=t("doc-page");return r(),d(n,{title:"Checkbox"},{default:o(()=>[e(a,{name:"pf-checkbox"}),e(i,{title:"Default",source:'<pf-checkbox id="basic-checkbox" label="Basic checkbox" />'},{default:o(()=>[e(c,{id:"basic-checkbox",label:"Basic checkbox"})]),_:1}),e(i,{title:"With description",source:`<pf-checkbox
+id="checkbox-with-description"
+label="Checkbox with description"
+description="Single-tenant cloud service hosted and managed by Red Hat that offers high-availability enterprise-grade clusters in a virtual private cloud on AWS or GCP." />`},{default:o(()=>[e(c,{id:"checkbox-with-description",label:"Checkbox with description",description:"Single-tenant cloud service hosted and managed by Red Hat that offers high-availability enterprise-grade clusters in a virtual private cloud on AWS or GCP."})]),_:1}),e(i,{title:"With description and body",source:`<pf-checkbox
+id="checkbox-with-description-and-body"
+label="Checkbox with description and body"
+description="Single-tenant cloud service hosted and managed by Red Hat that offers high-availability enterprise-grade clusters in a virtual private cloud on AWS or GCP."
+body="This is where custom content goes." />`},{default:o(()=>[e(c,{id:"checkbox-with-description-and-body",label:"Checkbox with description and body",description:"Single-tenant cloud service hosted and managed by Red Hat that offers high-availability enterprise-grade clusters in a virtual private cloud on AWS or GCP.",body:"This is where custom content goes."})]),_:1})]),_:1})}const _=s(l,[["render",h]]);export{_ as default};
