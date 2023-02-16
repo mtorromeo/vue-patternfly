e4980c250301c28a193304782f948e6dea301287
mtorromeo <massimiliano.torromeo@gmail.com>
2023-02-16 09:21:02 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly4@68bdfa999f1035ced80590e7fb1ce3c71a995a5c 🚀

---
+++ assets/Spinner.story-7bd10170.js
@@ -0,0 +1,4 @@
+import{_ as p,a as s,o as r,c as a,w as t,b as e}from"./index-ae4712f4.js";const _={};function f(l,m){const c=s("component-title"),n=s("pf-spinner"),o=s("story-canvas"),i=s("doc-page");return r(),a(i,{title:"Spinner"},{default:t(()=>[e(c,{name:"pf-spinner"}),e(o,{title:"Basic",source:"<pf-spinner />"},{default:t(()=>[e(n)]),_:1}),e(o,{title:"Size variations",source:`<pf-spinner size="sm" />
+<pf-spinner size="md" />
+<pf-spinner size="lg" />
+<pf-spinner size="xl" />`},{default:t(()=>[e(n,{size:"sm"}),e(n,{size:"md"}),e(n,{size:"lg"}),e(n,{size:"xl"})]),_:1})]),_:1})}const z=p(_,[["render",f]]);export{z as default};
