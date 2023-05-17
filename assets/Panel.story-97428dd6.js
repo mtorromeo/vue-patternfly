0e0eec28d971fb6a21468e7af1eaa94221659384
mtorromeo <massimiliano.torromeo@gmail.com>
2023-05-17 14:56:29 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@ea5e433fa4241fe47b696e0571f0b6442f86c16a 🚀

---
+++ assets/Panel.story-97428dd6.js
@@ -0,0 +1,132 @@
+import{e as b,r as s,a as r,o as h,c as M,w as e,b as l,g as t,d as n}from"./index-1db6e39c.js";const m=n("div",{class:"markdown pf-c-content"},[n("p",null,[t("The "),n("strong",null,"panel"),t(" component is a container that supports flexible content layouts. It can be used to house other components such as fields, forms, videos, buttons, and more. The panel should not be confused with the drawer component, which allows you to surface information via a collapsable container.")])],-1),y=n("br",null,null,-1),v=n("br",null,null,-1),w=n("br",null,null,-1),H=n("br",null,null,-1),F=n("br",null,null,-1),x=n("br",null,null,-1),B=n("br",null,null,-1),g=n("br",null,null,-1),N=n("br",null,null,-1),k=n("br",null,null,-1),C=n("br",null,null,-1),T=n("br",null,null,-1),V=n("br",null,null,-1),P=n("br",null,null,-1),S=n("br",null,null,-1),I=n("br",null,null,-1),R=n("br",null,null,-1),j=n("br",null,null,-1),q=n("br",null,null,-1),z=n("br",null,null,-1),A=n("br",null,null,-1),D=n("br",null,null,-1),E=n("br",null,null,-1),G=n("br",null,null,-1),J=n("br",null,null,-1),K=n("br",null,null,-1),L=n("br",null,null,-1),O=n("br",null,null,-1),Q=n("br",null,null,-1),U=n("br",null,null,-1),W=n("br",null,null,-1),X=n("br",null,null,-1),Y=n("br",null,null,-1),Z=n("br",null,null,-1),$=n("br",null,null,-1),nn=n("br",null,null,-1),ln=n("br",null,null,-1),tn=n("br",null,null,-1),en=n("br",null,null,-1),on=n("br",null,null,-1),an=n("br",null,null,-1),rn=n("br",null,null,-1),cn=n("br",null,null,-1),un=n("br",null,null,-1),_n=n("br",null,null,-1),pn=n("br",null,null,-1),sn=n("br",null,null,-1),dn=n("br",null,null,-1),fn=n("br",null,null,-1),bn=n("br",null,null,-1),hn=n("br",null,null,-1),Mn=n("br",null,null,-1),mn=n("br",null,null,-1),yn=n("br",null,null,-1),vn=n("br",null,null,-1),wn=n("br",null,null,-1),gn=b({__name:"Panel.story",setup(Hn){return s(1),s(10),(Fn,xn)=>{const d=r("component-title"),c=r("pf-panel-main-body"),o=r("pf-panel-main"),a=r("pf-panel"),i=r("story-canvas"),u=r("pf-panel-header"),_=r("pf-divider"),p=r("pf-panel-footer"),f=r("doc-page");return h(),M(f,{title:"Panel"},{default:e(()=>[l(d,{name:"pf-panel"}),m,l(i,{title:"Basic",source:`<pf-panel>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Header",source:`<pf-panel>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Footer",source:`<pf-panel>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1}),l(i,{title:"Header and footer",source:`<pf-panel>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1}),l(i,{title:"No body",source:`<pf-panel>
+  <pf-panel-main>Main content</pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1}),l(i,{title:"Raised",source:`<pf-panel variant="raised">
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{variant:"raised"},{default:e(()=>[l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Bordered",source:`<pf-panel variant="bordered">
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{variant:"bordered"},{default:e(()=>[l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Scrollable",source:`<pf-panel scrollable>
+  <pf-panel-main>
+    <pf-panel-main-body>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+    </pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{scrollable:""},{default:e(()=>[l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t(" Main content "),y,v,t(" Main content "),w,H,t(" Main content "),F,x,t(" Main content "),B,g,t(" Main content "),N,k,t(" Main content "),C,T,t(" Main content "),V,P,t(" Main content "),S,I,t(" Main content "),R,j,t(" Main content "),q,z,t(" Main content "),A,D,t(" Main content "),E,G,t(" Main content "),J,K,t(" Main content "),L,O,t(" Main content ")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Scrollable with header and footer",source:`<pf-panel scrollable>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+    </pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,{scrollable:""},{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(c,null,{default:e(()=>[t(" Main content "),Q,U,t(" Main content "),W,X,t(" Main content "),Y,Z,t(" Main content "),$,nn,t(" Main content "),ln,tn,t(" Main content "),en,on,t(" Main content "),an,rn,t(" Main content "),cn,un,t(" Main content "),_n,pn,t(" Main content "),sn,dn,t(" Main content "),fn,bn,t(" Main content "),hn,Mn,t(" Main content "),mn,yn,t(" Main content "),vn,wn,t(" Main content ")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1})]),_:1})}}});export{gn as default};
