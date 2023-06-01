391bf0fa8b7fc497df7812fb1fc8965c9157aa07
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2023-06-01 09:10:41 +0200

    feat(TextInputGroupUtilities): declared fallthrough attributes

--- packages/core/src/components/TextInputGroup/TextInputGroupUtilities.vue
+++ packages/core/src/components/TextInputGroup/TextInputGroupUtilities.vue
@@ -6,8 +6,14 @@
 
 <script lang="ts" setup>
 import styles from '@patternfly/react-styles/css/components/TextInputGroup/text-input-group';
+import type { HTMLAttributes } from 'vue';
 
 defineOptions({
   name: 'PfTextInputGroupUtilities',
 });
+
+export interface Props extends /* @vue-ignore */ HTMLAttributes {
+}
+
+defineProps<Props>();
 </script>
