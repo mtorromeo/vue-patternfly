92e8fab1ad8c4ae797b71afe9538e9e365c9cd6c
mtorromeo <massimiliano.torromeo@gmail.com>
2023-06-12 14:12:41 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@ec8f88081868a538cb49cab1d0df0dfa5fde1d5c 🚀

--- assets/Badge.story-5e67e184.js
+++
@@ -1,7 +0,0 @@
-import{_ as p,a as _,o as c,c as l,w as e,b as a,g as t}from"./index-7aa190b1.js";const s={};function r(g,u){const o=_("component-title"),d=_("pf-badge"),n=_("story-canvas"),f=_("doc-page");return c(),l(f,{title:"Badge"},{default:e(()=>[a(o,{name:"pf-badge"}),a(n,{title:"Read",source:`<pf-badge read>7</pf-badge>
-<pf-badge read>24</pf-badge>
-<pf-badge read>240</pf-badge>
-<pf-badge read>999+</pf-badge>`},{default:e(()=>[a(d,{read:""},{default:e(()=>[t("7")]),_:1}),a(d,{read:""},{default:e(()=>[t("24")]),_:1}),a(d,{read:""},{default:e(()=>[t("240")]),_:1}),a(d,{read:""},{default:e(()=>[t("999+")]),_:1})]),_:1}),a(n,{title:"Unread",source:`<pf-badge>7</pf-badge>
-<pf-badge>24</pf-badge>
-<pf-badge>240</pf-badge>
-<pf-badge>999+</pf-badge>`},{default:e(()=>[a(d,null,{default:e(()=>[t("7")]),_:1}),a(d,null,{default:e(()=>[t("24")]),_:1}),a(d,null,{default:e(()=>[t("240")]),_:1}),a(d,null,{default:e(()=>[t("999+")]),_:1})]),_:1})]),_:1})}const m=p(s,[["render",r],["__scopeId","data-v-78d3f680"]]);export{m as default};
---
+++ assets/Panel.story-c0bcda7d.js
@@ -0,0 +1,132 @@
+import{e as f,a as c,o as b,c as h,w as e,b as l,g as t,d as n}from"./index-cba05794.js";const M=n("div",{class:"markdown pf-c-content"},[n("p",null,[t("The "),n("strong",null,"panel"),t(" component is a container that supports flexible content layouts. It can be used to house other components such as fields, forms, videos, buttons, and more. The panel should not be confused with the drawer component, which allows you to surface information via a collapsable container.")])],-1),m=n("br",null,null,-1),y=n("br",null,null,-1),v=n("br",null,null,-1),w=n("br",null,null,-1),H=n("br",null,null,-1),F=n("br",null,null,-1),x=n("br",null,null,-1),B=n("br",null,null,-1),g=n("br",null,null,-1),N=n("br",null,null,-1),k=n("br",null,null,-1),C=n("br",null,null,-1),T=n("br",null,null,-1),V=n("br",null,null,-1),P=n("br",null,null,-1),S=n("br",null,null,-1),I=n("br",null,null,-1),R=n("br",null,null,-1),j=n("br",null,null,-1),q=n("br",null,null,-1),z=n("br",null,null,-1),A=n("br",null,null,-1),D=n("br",null,null,-1),E=n("br",null,null,-1),G=n("br",null,null,-1),J=n("br",null,null,-1),K=n("br",null,null,-1),L=n("br",null,null,-1),O=n("br",null,null,-1),Q=n("br",null,null,-1),U=n("br",null,null,-1),W=n("br",null,null,-1),X=n("br",null,null,-1),Y=n("br",null,null,-1),Z=n("br",null,null,-1),$=n("br",null,null,-1),nn=n("br",null,null,-1),ln=n("br",null,null,-1),tn=n("br",null,null,-1),en=n("br",null,null,-1),on=n("br",null,null,-1),an=n("br",null,null,-1),cn=n("br",null,null,-1),rn=n("br",null,null,-1),un=n("br",null,null,-1),_n=n("br",null,null,-1),pn=n("br",null,null,-1),sn=n("br",null,null,-1),dn=n("br",null,null,-1),fn=n("br",null,null,-1),bn=n("br",null,null,-1),hn=n("br",null,null,-1),Mn=n("br",null,null,-1),mn=n("br",null,null,-1),yn=n("br",null,null,-1),vn=n("br",null,null,-1),Bn=f({__name:"Panel.story",setup(wn){return(Hn,Fn)=>{const s=c("component-title"),r=c("pf-panel-main-body"),o=c("pf-panel-main"),a=c("pf-panel"),i=c("story-canvas"),u=c("pf-panel-header"),_=c("pf-divider"),p=c("pf-panel-footer"),d=c("doc-page");return b(),h(d,{title:"Panel"},{default:e(()=>[l(s,{name:"pf-panel"}),M,l(i,{title:"Basic",source:`<pf-panel>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Header",source:`<pf-panel>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Footer",source:`<pf-panel>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1}),l(i,{title:"Header and footer",source:`<pf-panel>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1}),l(i,{title:"No body",source:`<pf-panel>
+  <pf-panel-main>Main content</pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,null,{default:e(()=>[l(o,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1}),l(i,{title:"Raised",source:`<pf-panel variant="raised">
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{variant:"raised"},{default:e(()=>[l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Bordered",source:`<pf-panel variant="bordered">
+  <pf-panel-main>
+    <pf-panel-main-body>Main content</pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{variant:"bordered"},{default:e(()=>[l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t("Main content")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Scrollable",source:`<pf-panel scrollable>
+  <pf-panel-main>
+    <pf-panel-main-body>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+    </pf-panel-main-body>
+  </pf-panel-main>
+</pf-panel>`},{default:e(()=>[l(a,{scrollable:""},{default:e(()=>[l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t(" Main content "),m,y,t(" Main content "),v,w,t(" Main content "),H,F,t(" Main content "),x,B,t(" Main content "),g,N,t(" Main content "),k,C,t(" Main content "),T,V,t(" Main content "),P,S,t(" Main content "),I,R,t(" Main content "),j,q,t(" Main content "),z,A,t(" Main content "),D,E,t(" Main content "),G,J,t(" Main content "),K,L,t(" Main content ")]),_:1})]),_:1})]),_:1})]),_:1}),l(i,{title:"Scrollable with header and footer",source:`<pf-panel scrollable>
+  <pf-panel-header>Header content</pf-panel-header>
+  <pf-divider></pf-divider>
+  <pf-panel-main>
+    <pf-panel-main-body>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+      <br>
+      <br>
+      Main content
+    </pf-panel-main-body>
+  </pf-panel-main>
+  <pf-panel-footer>Footer content</pf-panel-footer>
+</pf-panel>`},{default:e(()=>[l(a,{scrollable:""},{default:e(()=>[l(u,null,{default:e(()=>[t("Header content")]),_:1}),l(_),l(o,null,{default:e(()=>[l(r,null,{default:e(()=>[t(" Main content "),O,Q,t(" Main content "),U,W,t(" Main content "),X,Y,t(" Main content "),Z,$,t(" Main content "),nn,ln,t(" Main content "),tn,en,t(" Main content "),on,an,t(" Main content "),cn,rn,t(" Main content "),un,_n,t(" Main content "),pn,sn,t(" Main content "),dn,fn,t(" Main content "),bn,hn,t(" Main content "),Mn,mn,t(" Main content "),yn,vn,t(" Main content ")]),_:1})]),_:1}),l(p,null,{default:e(()=>[t("Footer content")]),_:1})]),_:1})]),_:1})]),_:1})}}});export{Bn as default};
