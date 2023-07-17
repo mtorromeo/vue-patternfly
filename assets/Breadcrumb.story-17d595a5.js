11822b920a77ed48d407058cfac2ade178a534bd
mtorromeo <massimiliano.torromeo@gmail.com>
2023-07-17 17:22:53 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@00e05152f2977ff027db994c27d5e5811c8a1d2d 🚀

--- assets/Breadcrumb.story-17d595a5.js
+++
@@ -1,6 +0,0 @@
-import{_ as i,a as r,o as b,c as d,w as e,b as t,g as a}from"./index-d4702c97.js";const _={};function p(u,s){const n=r("component-title"),c=r("pf-breadcrumb-item"),o=r("pf-breadcrumb"),m=r("story-canvas"),f=r("doc-page");return b(),d(f,{title:"Breadcrumb"},{default:e(()=>[t(n,{name:"pf-breadcrumb"}),t(m,{title:"Example",source:`<pf-breadcrumb>
-  <pf-breadcrumb-item href="#">Section home</pf-breadcrumb-item>
-  <pf-breadcrumb-item href="#">Section title</pf-breadcrumb-item>
-  <pf-breadcrumb-item href="#">Section title</pf-breadcrumb-item>
-  <pf-breadcrumb-item href="#" active>Section landing</pf-breadcrumb-item>
-</pf-breadcrumb>`},{default:e(()=>[t(o,null,{default:e(()=>[t(c,{href:"#"},{default:e(()=>[a("Section home")]),_:1}),t(c,{href:"#"},{default:e(()=>[a("Section title")]),_:1}),t(c,{href:"#"},{default:e(()=>[a("Section title")]),_:1}),t(c,{href:"#",active:""},{default:e(()=>[a("Section landing")]),_:1})]),_:1})]),_:1})]),_:1})}const h=i(_,[["render",p]]);export{h as default};
