3de48e89ef3908176942f9c4c3fcd81b669757ac
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2023-11-08 16:53:09 +0100

    feat(Masthead): allow masthead to be user as a router-link (added `to` and `replace` props)

--- packages/core/src/components/Masthead/MastheadBrand.vue
+++ packages/core/src/components/Masthead/MastheadBrand.vue
@@ -1,16 +1,30 @@
 <template>
   <component
-    :is="component ?? (href === undefined ? 'span' : 'a')"
-    :class="styles.mastheadBrand"
-    :tabindex="(component === 'a' || href !== undefined) ? 0 : undefined"
+    :is="to ? 'router-link' : (PassThrough as Component)"
+    v-slot="routerCtx"
+    :to="to"
+    :replace="replace"
+    custom
   >
-    <slot />
+    <component
+      :is="component"
+      :class="styles.mastheadBrand"
+      :tabindex="component === 'a' ? 0 : undefined"
+      :href="(routerCtx as RouterLinkContext|undefined)?.href ?? href"
+      @click="(routerCtx as RouterLinkContext|undefined)?.navigate"
+    >
+      <slot />
+    </component>
   </component>
 </template>
 
 <script lang="ts" setup>
 import styles from '@patternfly/react-styles/css/components/Masthead/masthead';
-import type { AnchorHTMLAttributes } from 'vue';
+import PassThrough from '../../helpers/PassThrough.vue';
+import type { AnchorHTMLAttributes, Component, UnwrapRef } from 'vue';
+import type { useLink, RouteLocationRaw } from 'vue-router';
+
+type RouterLinkContext = UnwrapRef<ReturnType<typeof useLink>>;
 
 defineOptions({
   name: 'PfMastheadBrand',
@@ -19,11 +33,15 @@ defineOptions({
 export interface Props extends /* @vue-ignore */ AnchorHTMLAttributes {
   component?: string;
   href?: string;
+  to?: RouteLocationRaw;
+  replace?: boolean;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
 
 defineSlots<{
   default?: (props?: Record<never, never>) => any;
 }>();
+
+const component = props.component ?? ((props.href ?? props.to) === undefined ? 'span' : 'a');
 </script>
