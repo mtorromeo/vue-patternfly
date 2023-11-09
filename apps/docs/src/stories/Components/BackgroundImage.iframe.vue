7bc29fe544fee118712e46aea6359a6fc4953170
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2023-11-09 09:24:03 +0100

    docs: added actual background image to the pf-background-image story

--- apps/docs/src/stories/Components/BackgroundImage.iframe.vue
+++ apps/docs/src/stories/Components/BackgroundImage.iframe.vue
@@ -1,3 +1,7 @@
 <template>
-  <pf-background-image />
+  <pf-background-image :src="PfBgIcon" />
 </template>
+
+<script setup lang="ts">
+import PfBgIcon from '@patternfly/patternfly/assets/images/pfbg-icon.svg';
+</script>
