f325695b5743b027823a609123fc0a076b503d52
mtorromeo <massimiliano.torromeo@gmail.com>
2023-11-25 11:37:50 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@4f7b031874eb46a0e61b55e078b5581b3eef7ec0 🚀

---
+++ assets/Masthead.story-KbsaQcWD.js
@@ -0,0 +1,79 @@
+import{e as b,a as t,o as v,c as x,w as n,b as a,u as m,B as _,g as e,d as l}from"./index-aaYEbCQ_.js";const L=l("div",{class:"markdown pf-v5-c-content"},[l("p",null,[e("A "),l("strong",null,"masthead"),e(" contains global properties such as logotype, navigation and settings in an organized fashion and it is accessible from all parts of the application. All pages should share a common masthead.")]),l("h2",{class:"pf-v5-c-title"},"Examples")],-1),B=l("span",null,"Content",-1),C=l("span",null,"Testing text color",-1),y=l("span",null,"Content",-1),G=l("span",null,"Content",-1),k=l("span",null,"Content",-1),I=l("span",null,"Content",-1),A=b({__name:"Masthead.story",setup(w){return(N,T)=>{const c=t("component-title"),o=t("pf-button"),s=t("pf-masthead-toggle"),d=t("pf-masthead-brand"),p=t("pf-masthead-main"),f=t("pf-masthead-content"),i=t("pf-masthead"),u=t("story-canvas"),h=t("pf-flex-item"),r=t("pf-flex"),g=t("doc-page");return v(),x(g,{title:"Masthead"},{default:n(()=>[a(c,{name:"pf-masthead"}),L,a(u,{title:"Basic",source:`<pf-masthead>
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,null,{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[B]),_:1})]),_:1})]),_:1}),a(u,{title:"Basic with mixed content",source:`<pf-masthead>
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <pf-flex>
+      <span>Testing text color</span>
+      <pf-button>testing</pf-button>
+      <pf-flex-item align-self="flex-end">
+        <pf-button>testing</pf-button>
+      </pf-flex-item>
+    </pf-flex>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,null,{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[a(r,null,{default:n(()=>[C,a(o,null,{default:n(()=>[e("testing")]),_:1}),a(h,{"align-self":"flex-end"},{default:n(()=>[a(o,null,{default:n(()=>[e("testing")]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})]),_:1}),a(u,{title:"Display stack, display inline responsive",source:`<pf-masthead display="inline" display-lg="stack" display-xl2="inline">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,{display:"inline","display-lg":"stack","display-xl2":"inline"},{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[y]),_:1})]),_:1})]),_:1}),a(u,{title:"Light variant",source:`<pf-masthead background-color="light">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,{"background-color":"light"},{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[G]),_:1})]),_:1})]),_:1}),a(u,{title:"Light200 variant",source:`<pf-masthead background-color="light200">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,{"background-color":"light200"},{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[k]),_:1})]),_:1})]),_:1}),a(u,{title:"Inset",source:`<pf-masthead inset="sm">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:n(()=>[a(i,{inset:"sm"},{default:n(()=>[a(s,null,{default:n(()=>[a(o,{variant:"plain","aria-label":"Global navigation"},{default:n(()=>[a(m(_))]),_:1})]),_:1}),a(p,null,{default:n(()=>[a(d,null,{default:n(()=>[e("Logo")]),_:1})]),_:1}),a(f,null,{default:n(()=>[I]),_:1})]),_:1})]),_:1})]),_:1})}}});export{A as default};
---
+++ assets/TextInput.story-ZxYITJnT.js
@@ -0,0 +1,48 @@
+import{m,e as x,r as s,a as o,o as v,c as b,w as t,b as e,u as f,d as u}from"./index-aaYEbCQ_.js";const _={name:"ClockIcon",height:512,width:512,svgPath:"M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z",yOffset:0,xOffset:0},h=m(_),q=h,g=u("br",null,null,-1),y=u("br",null,null,-1),V=u("br",null,null,-1),C=u("br",null,null,-1),B=x({__name:"TextInput.story",setup(k){const d=s(""),r=s("");return(I,n)=>{const p=o("component-title"),a=o("pf-text-input"),l=o("story-canvas"),c=o("doc-page");return v(),b(c,{title:"Text Input"},{default:t(()=>[e(p,{name:"pf-text-input"}),e(l,{title:"Basic",source:'<pf-text-input v-model="text1" aria-label="text input example" />'},{default:t(()=>[e(a,{modelValue:d.value,"onUpdate:modelValue":n[0]||(n[0]=i=>d.value=i),"aria-label":"text input example"},null,8,["modelValue"])]),_:1}),e(l,{title:"Disabled",source:`<pf-text-input
+disabled
+value="disabled text input example"
+aria-label="disabled text input example" />`},{default:t(()=>[e(a,{disabled:"",value:"disabled text input example","aria-label":"disabled text input example"})]),_:1}),e(l,{title:"Read only",source:`<pf-text-input
+readonly
+value="read only text input example"
+aria-label="read only text input example" />`},{default:t(()=>[e(a,{readonly:"",value:"read only text input example","aria-label":"read only text input example"})]),_:1}),e(l,{title:"Validation",source:`<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (does not validate)"
+  aria-label="does not validate"
+  :auto-validate="false" />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on enter/submit)"
+  aria-label="validates on enter/submit"
+  auto-validate />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on blur)"
+  aria-label="validates on blur"
+  auto-validate="blur" />
+<br>
+<pf-text-input
+  v-model="text2"
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on change)"
+  aria-label="validates on change"
+  auto-validate="change" />
+<br>
+<pf-text-input
+  required
+  pattern="[0-9a-f]*"
+  placeholder="hexadecimal value (validates on input)"
+  aria-label="validates on input"
+  auto-validate="input" />`},{default:t(()=>[e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (does not validate)","aria-label":"does not validate","auto-validate":!1}),g,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on enter/submit)","aria-label":"validates on enter/submit","auto-validate":""}),y,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on blur)","aria-label":"validates on blur","auto-validate":"blur"}),V,e(a,{modelValue:r.value,"onUpdate:modelValue":n[1]||(n[1]=i=>r.value=i),required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on change)","aria-label":"validates on change","auto-validate":"change"},null,8,["modelValue"]),C,e(a,{required:"",pattern:"[0-9a-f]*",placeholder:"hexadecimal value (validates on input)","aria-label":"validates on input","auto-validate":"input"})]),_:1}),e(l,{title:"Truncated on left",source:'<pf-text-input start-truncated model-value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />'},{default:t(()=>[e(a,{"start-truncated":"","model-value":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."})]),_:1}),e(l,{title:"Select all",source:`<pf-text-input
+value="select all on click"
+aria-label="select-all"
+@focus="($event.currentTarget as HTMLInputElement).select()" />`},{default:t(()=>[e(a,{value:"select all on click","aria-label":"select-all",onFocus:n[2]||(n[2]=i=>i.currentTarget.select())})]),_:1}),e(l,{title:"Custom icon",source:`<pf-text-input aria-label="text input example">
+  <template #icon>
+    <ClockIcon />
+  </template>
+</pf-text-input>`},{default:t(()=>[e(a,{"aria-label":"text input example"},{icon:t(()=>[e(f(q))]),_:1})]),_:1})]),_:1})}}});export{B as default};
