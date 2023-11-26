545df2db656fca5f51447324203ab6b5aae3f06e
mtorromeo <massimiliano.torromeo@gmail.com>
2023-11-26 19:40:38 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@6c1612e69d8f8bd1ad4af5da7509a6de8eb40c6d 🚀

--- assets/Badge.story-wFYds88Q.js
+++
@@ -1,7 +0,0 @@
-import{_ as p,a as n,o as c,c as l,w as e,b as a,g as t}from"./index-aHpSD51e.js";const r={};function s(g,b){const f=n("component-title"),d=n("pf-badge"),o=n("story-canvas"),_=n("doc-page");return c(),l(_,{title:"Badge"},{default:e(()=>[a(f,{name:"pf-badge"}),a(o,{title:"Read",source:`<pf-badge read>7</pf-badge>
-<pf-badge read>24</pf-badge>
-<pf-badge read>240</pf-badge>
-<pf-badge read>999+</pf-badge>`},{default:e(()=>[a(d,{read:""},{default:e(()=>[t("7")]),_:1}),a(d,{read:""},{default:e(()=>[t("24")]),_:1}),a(d,{read:""},{default:e(()=>[t("240")]),_:1}),a(d,{read:""},{default:e(()=>[t("999+")]),_:1})]),_:1}),a(o,{title:"Unread",source:`<pf-badge>7</pf-badge>
-<pf-badge>24</pf-badge>
-<pf-badge>240</pf-badge>
-<pf-badge>999+</pf-badge>`},{default:e(()=>[a(d,null,{default:e(()=>[t("7")]),_:1}),a(d,null,{default:e(()=>[t("24")]),_:1}),a(d,null,{default:e(()=>[t("240")]),_:1}),a(d,null,{default:e(()=>[t("999+")]),_:1})]),_:1})]),_:1})}const m=p(r,[["render",s],["__scopeId","data-v-6db77e47"]]);export{m as default};
