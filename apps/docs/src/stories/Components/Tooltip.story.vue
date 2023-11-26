6c1612e69d8f8bd1ad4af5da7509a6de8eb40c6d
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2023-11-26 20:30:24 +0100

    feat(docs): added Tooltip story

---
+++ apps/docs/src/stories/Components/Tooltip.story.vue
@@ -0,0 +1,24 @@
+<template>
+  <doc-page title="Tooltip">
+    <template #description>A <b>tooltip</b> is in-app messaging used to identify elements on a page with short, clarifying text.</template>
+
+    <template #apidocs>
+      <component-info src="packages/core/src/components/Tooltip/Tooltip.vue" />
+      <component-info src="packages/core/src/components/Tooltip/TooltipArrow.vue" />
+      <component-info src="packages/core/src/components/Tooltip/TooltipContent.vue" />
+    </template>
+
+    <pre v-md>
+      ## Examples
+    </pre>
+
+    <story-canvas title="Basic">
+      <div style="margin: 100px">
+        <pf-tooltip>
+          <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis.</template>
+          <pf-button>I have a tooltip!</pf-button>
+        </pf-tooltip>
+      </div>
+    </story-canvas>
+  </doc-page>
+</template>
