43155985a49bc36b342272368463dd528bf0a7e8
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2023-12-12 12:18:36 +0100

    feat(Tooltip): implemented focus, click and manual triggers

--- apps/docs/src/stories/Components/Tooltip.story.vue
+++ apps/docs/src/stories/Components/Tooltip.story.vue
@@ -13,12 +13,60 @@
     </pre>
 
     <story-canvas title="Basic">
-      <div style="margin: 100px">
+      <div style="margin: 6rem">
         <pf-tooltip>
           <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis.</template>
           <pf-button>I have a tooltip!</pf-button>
         </pf-tooltip>
       </div>
     </story-canvas>
+
+    <story-canvas title="Options">
+      <pf-form horizontal>
+        <pf-form-group label="Trigger">
+          <pf-checkbox label="mouseenter" :model-value="trigger.includes('mouseenter')" @update:model-value="checkTrigger('mouseenter', $event)" />
+          <pf-checkbox label="focus" :model-value="trigger.includes('focus')" @update:model-value="checkTrigger('focus', $event)" />
+          <pf-checkbox label="click" :model-value="trigger.includes('click')" @update:model-value="checkTrigger('click', $event)" />
+        </pf-form-group>
+
+        <pf-form-group label="Content">
+          <pf-checkbox v-model="leftAligned" label="Left aligned" />
+        </pf-form-group>
+      </pf-form>
+
+      <div style="margin-top: 1rem; height: 20rem; width: 100%; overflow: auto; border: 1px dashed red">
+        <pf-bullseye style="height: 200%; width: 200%">
+          <pf-tooltip :trigger="trigger.join(' ')" :left-aligned="leftAligned">
+            <template #content>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis.</template>
+            <pf-button ref="buttonRef">I have a tooltip!</pf-button>
+          </pf-tooltip>
+        </pf-bullseye>
+      </div>
+    </story-canvas>
   </doc-page>
 </template>
+
+<script lang="ts" setup>
+import { reactive, ref, type Ref, onMounted } from 'vue';
+import { PfButton } from '@vue-patternfly/core';
+
+const buttonRef: Ref<InstanceType<typeof PfButton> | undefined> = ref();
+const trigger = reactive(['mouseenter', 'focus']);
+const leftAligned = ref(false);
+
+onMounted(() => {
+  buttonRef.value?.el?.scrollIntoView({
+    behavior: 'instant',
+    block: 'center',
+    inline: 'center',
+  });
+});
+
+function checkTrigger(value: string, checked: boolean) {
+  if (checked) {
+    trigger.push(value);
+  } else {
+    trigger.splice(trigger.indexOf(value), 1);
+  }
+}
+</script>
--- packages/core/src/components/Tooltip/Tooltip.vue
+++ packages/core/src/components/Tooltip/Tooltip.vue
@@ -3,15 +3,23 @@
     <slot />
   </pass-through>
 
-  <floating-ui v-if="$slots.content" :reference="referenceElement" :placement="placement" :offset="distance" flip>
+  <floating-ui
+    v-if="$slots.content"
+    v-slot="{ placement: floatingPlacement }"
+    :reference="referenceElement"
+    :placement="placement"
+    :offset="distance"
+    :flip="flip"
+  >
     <div
       ref="tooltipElement"
       v-bind="{...ouiaProps, ...$attrs}"
-      :class="[styles.tooltip, positionModifiers[placement]]"
+      :class="[styles.tooltip, positionModifiers[floatingPlacement]]"
       :style="{
         maxWidth,
-        opacity: visible ? 1 : 0,
+        opacity: managedVisible ? 1 : 0,
         transition: 'opacity 300ms cubic-bezier(0.54, 1.5, 0.38, 1.11) 0s',
+        display: tooltipDisplay ? 'initial' : 'none',
       }"
       role="tooltip"
       @transitionend="hide"
@@ -26,13 +34,13 @@
 
 <script lang="ts" setup>
 import styles from '@patternfly/react-styles/css/components/Tooltip/tooltip';
-import { type Ref, ref, watch, computed, type HTMLAttributes } from 'vue';
+import { type Ref, ref, watch, computed, onMounted, onUnmounted, type HTMLAttributes } from 'vue';
 
 import PfTooltipArrow from './TooltipArrow.vue';
 import PfTooltipContent from './TooltipContent.vue';
 import FloatingUi from '../../helpers/FloatingUi.vue';
 import PassThrough from '../../helpers/PassThrough.vue';
-import { useHtmlElementFromVNodes } from '../../use';
+import { useHtmlElementFromVNodes, useManagedProp } from '../../use';
 import type { Placement } from '@floating-ui/core';
 import { useOUIAProps, type OUIAProps } from '../../helpers/ouia';
 
@@ -45,15 +53,40 @@ defineOptions({
 
 export interface Props extends OUIAProps, /* @vue-ignore */ Omit<HTMLAttributes, 'role' | 'onTransitionend'> {
   position?: TooltipPosition;
+  /** A combination of the strings 'mouseenter', 'focus' and 'click' */
   trigger?: string;
+  /** Flag to indicate that the text content is left aligned */
   leftAligned?: boolean,
+  /** Delay in ms before the tooltip appears */
   entryDelay?: number;
+  /** Delay in ms before the tooltip disappears, Avoid passing in a value of "0", as users should
+   * be given ample time to move their mouse from the trigger to the tooltip content without the content
+   * being hidden.
+   */
   exitDelay?: number;
+  /** Distance of the tooltip to its target */
   distance?: number;
-  aria?: string;
+  /**
+   * aria-labelledby or aria-describedby for tooltip.
+   * The trigger will be cloned to add the aria attribute, and the corresponding id in the form of 'pf-tooltip-#' is added to the content container.
+   * If you don't want that or prefer to add the aria attribute yourself on the trigger, set aria to 'none'.
+   */
+  aria?: 'describedby' | 'labelledby' | 'none';
+  /** CSS fade transition animation duration */
   animationDuration?: number;
+  /** Minimum width of the tooltip. If set to "trigger", the minimum width will be set to the reference element width. */
+  minWidth?: string | 'trigger';
+  /** Maximum width of the tooltip */
   maxWidth?: number;
+  /** Tooltip content */
   content?: string;
+  /**
+   * The desired position to flip the tooltip to if the initial position is not possible.
+   * By setting this prop to 'flip' it attempts to flip the tooltip to the opposite side if there is no space.
+   */
+  flip?: boolean;
+  /** value for visibility when trigger is 'manual' */
+  visible?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -64,6 +97,8 @@ const props = withDefaults(defineProps<Props>(), {
   distance: 15,
   aria: 'describedby',
   animationDuration: 300,
+  flip: true,
+  visible: undefined,
 });
 const ouiaProps = useOUIAProps({id: props.ouiaId, safe: props.ouiaSafe});
 
@@ -72,9 +107,18 @@ defineSlots<{
   content?: (props?: Record<never, never>) => any;
 }>();
 
+defineEmits<{
+  (name: 'update:visible', value: boolean): void;
+}>();
+
 const { element: referenceElement, findReference } = useHtmlElementFromVNodes();
 const tooltipElement: Ref<HTMLElement | undefined> = ref();
-const visible = ref(false);
+const tooltipDisplay = ref(false);
+const managedVisible = useManagedProp('visible', false);
+
+const triggerMouseEnter = computed(() => props.trigger.split(' ').includes('mouseenter'));
+const triggerFocus = computed(() => props.trigger.split(' ').includes('focus'));
+const triggerClick = computed(() => props.trigger.split(' ').includes('click'));
 
 const positionModifiers: Record<Placement, string> = {
   top: styles.modifiers.top,
@@ -94,25 +138,75 @@ const positionModifiers: Record<Placement, string> = {
 // TODO: actual auto detect
 const placement = computed<Placement>(() => props.position === 'auto' ? 'top' : props.position);
 
-watch(tooltipElement, (el) => {
-  if (el && !visible.value) {
-    el.style.display = 'none';
+watch([tooltipElement, triggerClick, triggerFocus, triggerMouseEnter], hide);
+
+watch(managedVisible, () => {
+  if (managedVisible.value) {
+    tooltipDisplay.value = true;
   }
 });
 
-watch(referenceElement, (el) => {
-  el?.addEventListener('mouseenter', () => {
-    visible.value = true;
-    if (tooltipElement.value) {
-      tooltipElement.value.style.display = 'initial';
-    }
-  });
-  el?.addEventListener('mouseleave', () => (visible.value = false));
+watch(referenceElement, (el, oldEl) => {
+  oldEl?.removeEventListener('mouseenter', handleMouseEnter);
+  oldEl?.removeEventListener('mouseleave', handleMouseLeave);
+  oldEl?.removeEventListener('focus', handleFocus);
+  oldEl?.removeEventListener('blur', handleBlur);
+
+  el?.addEventListener('mouseenter', handleMouseEnter);
+  el?.addEventListener('mouseleave', handleMouseLeave);
+  el?.addEventListener('focus', handleFocus);
+  el?.addEventListener('blur', handleBlur);
 });
 
-function hide() {
-  if (!visible.value && tooltipElement.value) {
-    tooltipElement.value.style.display = 'none';
+onMounted(() => {
+  document.addEventListener('click', handleClick);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClick);
+  referenceElement.value?.addEventListener('mouseenter', handleMouseEnter);
+  referenceElement.value?.addEventListener('mouseleave', handleMouseLeave);
+  referenceElement.value?.removeEventListener('focus', handleFocus);
+  referenceElement.value?.removeEventListener('blur', handleBlur);
+});
+
+function handleClick(e: MouseEvent) {
+  if (triggerClick.value) {
+    managedVisible.value = e.target === referenceElement.value;
   }
 }
+
+function handleFocus() {
+  if (triggerFocus.value) {
+    managedVisible.value = true;
+  }
+}
+
+function handleBlur() {
+  if (triggerFocus.value) {
+    managedVisible.value = false;
+  }
+}
+
+function handleMouseEnter() {
+  if (triggerMouseEnter.value) {
+    managedVisible.value = true;
+  }
+}
+
+function handleMouseLeave() {
+  if (triggerMouseEnter.value) {
+    managedVisible.value = false;
+  }
+}
+
+function hide() {
+  if (!managedVisible.value) {
+    tooltipDisplay.value = false;
+  }
+}
+
+defineExpose({
+  el: tooltipElement,
+});
 </script>
