58b1b52e78fd8dcfce7beef9766c654730e07e04
mtorromeo <massimiliano.torromeo@gmail.com>
2024-01-16 10:59:52 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@e50063a7919cbbec5c5fb91d8364ac4f906a1488 🚀

---
+++ assets/OverflowMenu.story-CJ4RfOnN.js
@@ -0,0 +1,62 @@
+import{m as c,f as I,r as M,o as b,c as H,w as o,a as n,b as e,u as f,d as A,e as t}from"./index-rHWKLjOV.js";import{E as y}from"./ellipsis-vertical-icon-wWqsslKq.js";const k={name:"AlignLeftIcon",height:512,width:448,svgPath:"M288 64c0 17.7-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32H256c17.7 0 32 14.3 32 32zm0 256c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H256c17.7 0 32 14.3 32 32zM0 192c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z",yOffset:0,xOffset:0},C=c(k),q=C,S={name:"AlignCenterIcon",height:512,width:448,svgPath:"M352 64c0-17.7-14.3-32-32-32H128c-17.7 0-32 14.3-32 32s14.3 32 32 32H320c17.7 0 32-14.3 32-32zm96 128c0-17.7-14.3-32-32-32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32zM0 448c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32zM352 320c0-17.7-14.3-32-32-32H128c-17.7 0-32 14.3-32 32s14.3 32 32 32H320c17.7 0 32-14.3 32-32z",yOffset:0,xOffset:0},x=c(S),z=x,N={name:"AlignRightIcon",height:512,width:448,svgPath:"M448 64c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32zm0 256c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32zM0 192c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z",yOffset:0,xOffset:0},P=c(N),V=P,T=A("b",null,"overflow menu",-1),B=I({__name:"OverflowMenu.story",setup(F){const u=M(!1);return(G,p)=>{const l=t("component-info"),a=t("pf-overflow-menu-item"),s=t("pf-overflow-menu-group"),d=t("pf-overflow-menu-content"),v=t("pf-menu-toggle"),r=t("pf-overflow-menu-dropdown-item"),w=t("pf-dropdown"),_=t("pf-overflow-menu-control"),g=t("pf-overflow-menu"),h=t("story-canvas"),i=t("pf-button"),O=t("doc-page");return b(),H(O,{name:"Components/OverflowMenu.story.vue",title:"Overflow Menu"},{description:o(()=>[n("An "),T,n(" groups a set of actions in a responsive horizontal list to help declutter an interface. Actions can be set to persist or collapse into a kebab as the viewpoint shrinks. Overflow menus are useful in toolbars or other places where a group of actions is required.")]),apidocs:o(()=>[e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenu.vue",doc:{name:"PfOverflowMenu",exportName:"default",displayName:"OverflowMenu",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"breakpoint",required:!0,type:{name:"TSTypeOperator"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/OverflowMenu/OverflowMenu.vue"]}},null,8,["doc"]),e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenuContent.vue",doc:{name:"PfOverflowMenuContent",exportName:"default",displayName:"OverflowMenuContent",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"persistent",required:!1,type:{name:"boolean"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/OverflowMenu/OverflowMenuContent.vue"]}},null,8,["doc"]),e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenuControl.vue",doc:{name:"PfOverflowMenuControl",exportName:"default",displayName:"OverflowMenuControl",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"additionalOptions",required:!1,type:{name:"boolean"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/OverflowMenu/OverflowMenuControl.vue"]}},null,8,["doc"]),e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenuDropdownItem.vue"}),e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenuGroup.vue",doc:{name:"PfOverflowMenuGroup",exportName:"default",displayName:"OverflowMenuGroup",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"persistent",required:!1,type:{name:"boolean"}},{name:"type",required:!1,type:{name:"union",elements:[{name:'"button"'},{name:'"icon"'}]}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/OverflowMenu/OverflowMenuGroup.vue"]}},null,8,["doc"]),e(l,{src:"packages/core/src/components/OverflowMenu/OverflowMenuItem.vue",doc:{name:"PfOverflowMenuItem",exportName:"default",displayName:"OverflowMenuItem",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"persistent",required:!1,type:{name:"boolean"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/OverflowMenu/OverflowMenuItem.vue"]}},null,8,["doc"])]),default:o(()=>[e(h,{title:"Default",style:{"min-height":"350px"},source:`<pf-overflow-menu breakpoint="lg">
+  <pf-overflow-menu-content>
+    <pf-overflow-menu-item>Item 1</pf-overflow-menu-item>
+    <pf-overflow-menu-item>Item 2</pf-overflow-menu-item>
+    <pf-overflow-menu-group>
+      <pf-overflow-menu-item>Item 3</pf-overflow-menu-item>
+      <pf-overflow-menu-item>Item 4</pf-overflow-menu-item>
+      <pf-overflow-menu-item>Item 5</pf-overflow-menu-item>
+    </pf-overflow-menu-group>
+  </pf-overflow-menu-content>
+  <pf-overflow-menu-control>
+    <pf-dropdown v-model:open="open">
+      <template #toggle>
+        <pf-menu-toggle variant="plain">
+          <ellipsis-vertical-icon />
+        </pf-menu-toggle>
+      </template>
+      <pf-overflow-menu-dropdown-item shared>Item 1</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item shared>Item 2</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item shared>Item 3</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item shared>Item 4</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item shared>Item 5</pf-overflow-menu-dropdown-item>
+    </pf-dropdown>
+  </pf-overflow-menu-control>
+</pf-overflow-menu>`},{default:o(()=>[e(g,{breakpoint:"lg"},{default:o(()=>[e(d,null,{default:o(()=>[e(a,null,{default:o(()=>[n("Item 1")]),_:1}),e(a,null,{default:o(()=>[n("Item 2")]),_:1}),e(s,null,{default:o(()=>[e(a,null,{default:o(()=>[n("Item 3")]),_:1}),e(a,null,{default:o(()=>[n("Item 4")]),_:1}),e(a,null,{default:o(()=>[n("Item 5")]),_:1})]),_:1})]),_:1}),e(_,null,{default:o(()=>[e(w,{open:u.value,"onUpdate:open":p[0]||(p[0]=m=>u.value=m)},{toggle:o(()=>[e(v,{variant:"plain"},{default:o(()=>[e(f(y))]),_:1})]),default:o(()=>[e(r,{shared:""},{default:o(()=>[n("Item 1")]),_:1}),e(r,{shared:""},{default:o(()=>[n("Item 2")]),_:1}),e(r,{shared:""},{default:o(()=>[n("Item 3")]),_:1}),e(r,{shared:""},{default:o(()=>[n("Item 4")]),_:1}),e(r,{shared:""},{default:o(()=>[n("Item 5")]),_:1})]),_:1},8,["open"])]),_:1})]),_:1})]),_:1}),e(h,{title:"Persistent",style:{"min-height":"350px"},source:`<pf-overflow-menu breakpoint="lg">
+  <pf-overflow-menu-content persistent>
+    <pf-overflow-menu-group type="button" persistent>
+      <pf-overflow-menu-item persistent>
+        <pf-button variant="primary">Primary</pf-button>
+      </pf-overflow-menu-item>
+      <pf-overflow-menu-item>
+        <pf-button variant="secondary">Secondary</pf-button>
+      </pf-overflow-menu-item>
+      <pf-overflow-menu-item>
+        <pf-button variant="tertiary">Tertiary</pf-button>
+      </pf-overflow-menu-item>
+    </pf-overflow-menu-group>
+    <pf-overflow-menu-group type="icon" persistent>
+      <pf-overflow-menu-item persistent>
+        <pf-button variant="plain"><align-left-icon /></pf-button>
+      </pf-overflow-menu-item>
+      <pf-overflow-menu-item persistent>
+        <pf-button variant="plain"><align-center-icon /></pf-button>
+      </pf-overflow-menu-item>
+      <pf-overflow-menu-item persistent>
+        <pf-button variant="plain"><align-right-icon /></pf-button>
+      </pf-overflow-menu-item>
+    </pf-overflow-menu-group>
+  </pf-overflow-menu-content>
+  <pf-overflow-menu-control additional-options>
+    <pf-dropdown v-model:open="open">
+      <template #toggle>
+        <pf-menu-toggle variant="plain">
+          <ellipsis-vertical-icon />
+        </pf-menu-toggle>
+      </template>
+      <pf-overflow-menu-dropdown-item shared>Secondary</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item shared>Tertiary</pf-overflow-menu-dropdown-item>
+      <pf-overflow-menu-dropdown-item>Action 4</pf-overflow-menu-dropdown-item>
+    </pf-dropdown>
+  </pf-overflow-menu-control>
+</pf-overflow-menu>`},{default:o(()=>[e(g,{breakpoint:"lg"},{default:o(()=>[e(d,{persistent:""},{default:o(()=>[e(s,{type:"button",persistent:""},{default:o(()=>[e(a,{persistent:""},{default:o(()=>[e(i,{variant:"primary"},{default:o(()=>[n("Primary")]),_:1})]),_:1}),e(a,null,{default:o(()=>[e(i,{variant:"secondary"},{default:o(()=>[n("Secondary")]),_:1})]),_:1}),e(a,null,{default:o(()=>[e(i,{variant:"tertiary"},{default:o(()=>[n("Tertiary")]),_:1})]),_:1})]),_:1}),e(s,{type:"icon",persistent:""},{default:o(()=>[e(a,{persistent:""},{default:o(()=>[e(i,{variant:"plain"},{default:o(()=>[e(f(q))]),_:1})]),_:1}),e(a,{persistent:""},{default:o(()=>[e(i,{variant:"plain"},{default:o(()=>[e(f(z))]),_:1})]),_:1}),e(a,{persistent:""},{default:o(()=>[e(i,{variant:"plain"},{default:o(()=>[e(f(V))]),_:1})]),_:1})]),_:1})]),_:1}),e(_,{"additional-options":""},{default:o(()=>[e(w,{open:u.value,"onUpdate:open":p[1]||(p[1]=m=>u.value=m)},{toggle:o(()=>[e(v,{variant:"plain"},{default:o(()=>[e(f(y))]),_:1})]),default:o(()=>[e(r,{shared:""},{default:o(()=>[n("Secondary")]),_:1}),e(r,{shared:""},{default:o(()=>[n("Tertiary")]),_:1}),e(r,null,{default:o(()=>[n("Action 4")]),_:1})]),_:1},8,["open"])]),_:1})]),_:1})]),_:1})]),_:1},8,["name"])}}});export{B as default};
