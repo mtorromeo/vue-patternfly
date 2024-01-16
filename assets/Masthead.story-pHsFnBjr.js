68439f8cb6e63873e99a65e75b91605b896e6c52
mtorromeo <massimiliano.torromeo@gmail.com>
2024-01-16 10:59:46 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@e50063a7919cbbec5c5fb91d8364ac4f906a1488 🚀

---
+++ assets/Masthead.story-pHsFnBjr.js
@@ -0,0 +1,79 @@
+import{f as b,o as v,c as y,w as a,a as t,b as e,u,B as f,d as s,e as n}from"./index-rHWKLjOV.js";const x=s("b",null,"masthead",-1),M=s("div",{class:"markdown pf-v5-c-content"},[s("h2",{class:"pf-v5-c-title"},"Examples")],-1),k=s("span",null,"Content",-1),q=s("span",null,"Testing text color",-1),C=s("span",null,"Content",-1),B=s("span",null,"Content",-1),I=s("span",null,"Content",-1),L=s("span",null,"Content",-1),O=b({__name:"Masthead.story",setup(w){return(N,G)=>{const c=n("component-info"),o=n("pf-button"),l=n("pf-masthead-toggle"),i=n("pf-masthead-brand"),d=n("pf-masthead-main"),m=n("pf-masthead-content"),p=n("pf-masthead"),r=n("story-canvas"),h=n("pf-flex-item"),g=n("pf-flex"),_=n("doc-page");return v(),y(_,{name:"Components/Masthead.story.vue",title:"Masthead"},{description:a(()=>[t("A "),x,t(" contains global properties such as logotype, navigation and settings in an organized fashion and it is accessible from all parts of the application. All pages should share a common masthead.")]),apidocs:a(()=>[e(c,{src:"packages/core/src/components/Masthead/Masthead.vue",doc:{name:"PfMasthead",exportName:"default",displayName:"Masthead",description:"",tags:{},props:[{name:"insetXl2",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"insetXl",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"insetLg",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"insetMd",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"insetSm",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"inset",required:!1,type:{name:"union",elements:[{name:'"none"'},{name:'"xs"'},{name:'"sm"'},{name:'"md"'},{name:'"lg"'},{name:'"xl"'},{name:'"2xl"'},{name:'"3xl"'}]}},{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"backgroundColor",description:"Background theme color of the masthead",required:!1,type:{name:"union",elements:[{name:'"dark"'},{name:'"light"'},{name:'"light200"'}]},defaultValue:{func:!1,value:"'dark'"}},{name:"display",description:"Display type at various breakpoints",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]},defaultValue:{func:!1,value:"'inline'"}},{name:"displaySm",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]}},{name:"displayMd",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]}},{name:"displayLg",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]}},{name:"displayXl",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]}},{name:"displayXl2",required:!1,type:{name:"union",elements:[{name:'"inline"'},{name:'"stack"'}]}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Masthead/Masthead.vue"]}},null,8,["doc"]),e(c,{src:"packages/core/src/components/Masthead/MastheadBrand.vue",doc:{name:"PfMastheadBrand",exportName:"default",displayName:"MastheadBrand",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"component",required:!1,type:{name:"string"}},{name:"href",required:!1,type:{name:"string"}},{name:"to",required:!1,type:{name:"RouteLocationRaw"}},{name:"replace",required:!1,type:{name:"boolean"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Masthead/MastheadBrand.vue"]}},null,8,["doc"]),e(c,{src:"packages/core/src/components/Masthead/MastheadContent.vue",doc:{name:"PfMastheadContent",exportName:"default",displayName:"MastheadContent",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Masthead/MastheadContent.vue"]}},null,8,["doc"]),e(c,{src:"packages/core/src/components/Masthead/MastheadMain.vue",doc:{name:"PfMastheadMain",exportName:"default",displayName:"MastheadMain",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Masthead/MastheadMain.vue"]}},null,8,["doc"]),e(c,{src:"packages/core/src/components/Masthead/MastheadToggle.vue",doc:{name:"PfMastheadToggle",exportName:"default",displayName:"MastheadToggle",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Masthead/MastheadToggle.vue"]}},null,8,["doc"])]),default:a(()=>[M,e(r,{title:"Basic",source:`<pf-masthead>
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,null,{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[k]),_:1})]),_:1})]),_:1}),e(r,{title:"Basic with mixed content",source:`<pf-masthead>
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <pf-flex>
+      <span>Testing text color</span>
+      <pf-button>testing</pf-button>
+      <pf-flex-item align-self="flex-end">
+        <pf-button>testing</pf-button>
+      </pf-flex-item>
+    </pf-flex>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,null,{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[e(g,null,{default:a(()=>[q,e(o,null,{default:a(()=>[t("testing")]),_:1}),e(h,{"align-self":"flex-end"},{default:a(()=>[e(o,null,{default:a(()=>[t("testing")]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})]),_:1}),e(r,{title:"Display stack, display inline responsive",source:`<pf-masthead display="inline" display-lg="stack" display-xl2="inline">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,{display:"inline","display-lg":"stack","display-xl2":"inline"},{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[C]),_:1})]),_:1})]),_:1}),e(r,{title:"Light variant",source:`<pf-masthead background-color="light">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,{"background-color":"light"},{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[B]),_:1})]),_:1})]),_:1}),e(r,{title:"Light200 variant",source:`<pf-masthead background-color="light200">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,{"background-color":"light200"},{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[I]),_:1})]),_:1})]),_:1}),e(r,{title:"Inset",source:`<pf-masthead inset="sm">
+  <pf-masthead-toggle>
+    <pf-button variant="plain" aria-label="Global navigation">
+      <BarsIcon />
+    </pf-button>
+  </pf-masthead-toggle>
+  <pf-masthead-main>
+    <pf-masthead-brand>Logo</pf-masthead-brand>
+  </pf-masthead-main>
+  <pf-masthead-content>
+    <span>Content</span>
+  </pf-masthead-content>
+</pf-masthead>`},{default:a(()=>[e(p,{inset:"sm"},{default:a(()=>[e(l,null,{default:a(()=>[e(o,{variant:"plain","aria-label":"Global navigation"},{default:a(()=>[e(u(f))]),_:1})]),_:1}),e(d,null,{default:a(()=>[e(i,null,{default:a(()=>[t("Logo")]),_:1})]),_:1}),e(m,null,{default:a(()=>[L]),_:1})]),_:1})]),_:1})]),_:1},8,["name"])}}});export{O as default};
