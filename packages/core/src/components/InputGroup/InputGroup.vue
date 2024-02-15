c08d7b80246ffaf2ce2f1b8e2792b6afdbf2809e
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2024-02-15 10:17:01 +0100

    fix(InputGroup): don't add aria-describedby automatically

--- packages/core/src/components/InputGroup/InputGroup.vue
+++ packages/core/src/components/InputGroup/InputGroup.vue
@@ -1,19 +1,13 @@
 <template>
   <div v-bind="(ouiaProps as any)" :class="styles.inputGroup">
-    <render />
+    <slot />
   </div>
 </template>
 
 <script lang="ts" setup>
 import styles from '@patternfly/react-styles/css/components/InputGroup/input-group';
 import { useOUIAProps, type OUIAProps } from '../../helpers/ouia';
-import { cloneVNode, type Component, type VNode, type HTMLAttributes } from 'vue';
-
-const formCtrls = ['PfFormSelect', 'PfTextArea', 'PfTextInput'];
-
-function vnodeIsFormCtrls(vnode: VNode) {
-  return typeof vnode.type === 'object' && formCtrls.includes((vnode.type as Component).name ?? '');
-}
+import { type VNode, type HTMLAttributes } from 'vue';
 
 defineOptions({
   name: 'PfInputGroup',
@@ -25,18 +19,7 @@ export interface Props extends OUIAProps, /* @vue-ignore */ HTMLAttributes {
 const props = defineProps<Props>();
 const ouiaProps = useOUIAProps({id: props.ouiaId, safe: props.ouiaSafe});
 
-const slots = defineSlots<{
+defineSlots<{
   default?: (props?: Record<never, never>) => VNode[];
 }>();
-
-function render() {
-  const children = slots.default?.({}) ?? [];
-  const idItem = children.find(child => !vnodeIsFormCtrls(child) && child.props?.id);
-
-  if (!idItem) {
-    return children;
-  }
-
-  return children.map(child => vnodeIsFormCtrls(child) ? cloneVNode(child, { 'aria-describedby': idItem.props?.id }) : child);
-}
 </script>
