76115f08ab7d2159eb4cc6f693fb69ad40f95803
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2024-02-15 10:00:28 +0100

    feat(Form): added maxWidth prop

--- packages/core/src/components/Form/Form.vue
+++ packages/core/src/components/Form/Form.vue
@@ -4,8 +4,9 @@
     :is="component"
     :class="[styles.form, {
       [styles.modifiers.horizontal]: horizontal,
-      [styles.modifiers.limitWidth]: widthLimited,
+      [styles.modifiers.limitWidth]: widthLimited || maxWidth,
     }]"
+    :style="{ [cssMaxWidth.name]: maxWidth }"
   >
     <slot />
   </component>
@@ -13,6 +14,7 @@
 
 <script lang="ts" setup>
 import styles from '@patternfly/react-styles/css/components/Form/form';
+import cssMaxWidth from '@patternfly/react-tokens/dist/esm/c_form_m_limit_width_MaxWidth';
 import type { Component, FormHTMLAttributes } from 'vue';
 import { useOUIAProps, type OUIAProps } from '../../helpers/ouia';
 
@@ -22,8 +24,12 @@ defineOptions({
 
 export interface Props extends OUIAProps, /* @vue-ignore */ FormHTMLAttributes {
   component?: string | Component;
+  /** Sets the Form to horizontal. */
   horizontal?: boolean;
+  /** Limits the max-width of the form. */
   widthLimited?: boolean;
+  /** Sets a custom max-width for the form. */
+  maxWidth?: string;
 }
 
 const props = withDefaults(defineProps<Props>(), {
