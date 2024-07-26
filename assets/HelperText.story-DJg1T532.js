e24a2368368725e41b15dcfd8b357acaa9c9def6
mtorromeo <massimiliano.torromeo@gmail.com>
2024-07-26 13:49:08 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@34dd0146b81f7859bd7a87f73e96ba12b1fa218e 🚀

---
+++ assets/HelperText.story-DJg1T532.js
@@ -0,0 +1,50 @@
+import{m as o,f,o as d,c as u,w as e,a as i,b as t,u as a,d as p,e as r}from"./index-D1ygUcxc.js";import{X as h}from"./xmark-icon-DuNsQ-SA.js";const x={name:"InfoIcon",height:512,width:192,svgPath:"M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32l64 0c17.7 0 32 14.3 32 32l0 224 32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 512c-17.7 0-32-14.3-32-32s14.3-32 32-32l32 0 0-192-32 0c-17.7 0-32-14.3-32-32z",yOffset:0,xOffset:0},v=o(x),_={name:"QuestionIcon",height:512,width:320,svgPath:"M80 160c0-35.3 28.7-64 64-64l32 0c35.3 0 64 28.7 64 64l0 3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74l0 1.4c0 17.7 14.3 32 32 32s32-14.3 32-32l0-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7l0-3.6c0-70.7-57.3-128-128-128l-32 0C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z",yOffset:0,xOffset:0},T=o(_),g={name:"ExclamationIcon",height:512,width:128,svgPath:"M96 64c0-17.7-14.3-32-32-32S32 46.3 32 64l0 256c0 17.7 14.3 32 32 32s32-14.3 32-32L96 64zM64 480a40 40 0 1 0 0-80 40 40 0 1 0 0 80z",yOffset:0,xOffset:0},y=o(g),w=p("b",null,"Helper text",-1),I=p("div",{class:"markdown pf-v5-c-content"},[p("h2",{class:"pf-v5-c-title"},"Examples")],-1),C=f({__name:"HelperText.story",setup(H){return(k,O)=>{const c=r("component-info"),n=r("pf-helper-text-item"),s=r("pf-helper-text"),l=r("story-canvas"),m=r("doc-page");return d(),u(m,{name:"Components/HelperText.story.vue",title:"Helper Text"},{description:e(()=>[w,i(" is an on-screen field guideline that helps provide context regarding field inputs.")]),apidocs:e(()=>[t(c,{src:"packages/core/src/components/HelperText/HelperText.vue",doc:{name:"PfHelperText",exportName:"default",displayName:"HelperText",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"component",required:!1,type:{name:"union",elements:[{name:'"div"'},{name:'"ul"'}]},defaultValue:{func:!1,value:"'div'"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/HelperText/HelperText.vue"]}}),t(c,{src:"packages/core/src/components/HelperText/HelperTextItem.vue",doc:{name:"PfHelperTextItem",exportName:"default",displayName:"HelperTextItem",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"variant",description:"Variant styling of the helper text item.",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"warning"'},{name:'"success"'},{name:'"error"'},{name:'"indeterminate"'}]},defaultValue:{func:!1,value:"'default'"}},{name:"icon",description:"Flag indicating the helper text should have an icon. Dynamic helper texts include icons by default while static helper texts do not.",required:!1,type:{name:"boolean"}},{name:"dynamic",description:"Flag indicating the helper text item is dynamic.",required:!1,type:{name:"boolean"}}],slots:[{name:"icon"},{name:"default"}],sourceFiles:["../../packages/core/src/components/HelperText/HelperTextItem.vue"]}})]),default:e(()=>[I,t(l,{title:"Static",source:`<pf-helper-text>
+  <pf-helper-text-item>This is default helper text</pf-helper-text-item>
+  <pf-helper-text-item variant="indeterminate">This is indeterminate helper text</pf-helper-text-item>
+  <pf-helper-text-item variant="warning">This is warning helper text</pf-helper-text-item>
+  <pf-helper-text-item variant="success">This is success helper text</pf-helper-text-item>
+  <pf-helper-text-item variant="error">This is error helper text</pf-helper-text-item>
+</pf-helper-text>`},{default:e(()=>[t(s,null,{default:e(()=>[t(n,null,{default:e(()=>[i("This is default helper text")]),_:1}),t(n,{variant:"indeterminate"},{default:e(()=>[i("This is indeterminate helper text")]),_:1}),t(n,{variant:"warning"},{default:e(()=>[i("This is warning helper text")]),_:1}),t(n,{variant:"success"},{default:e(()=>[i("This is success helper text")]),_:1}),t(n,{variant:"error"},{default:e(()=>[i("This is error helper text")]),_:1})]),_:1})]),_:1}),t(l,{title:"Static with default icons",source:`<pf-helper-text>
+  <pf-helper-text-item icon>This is default helper text</pf-helper-text-item>
+  <pf-helper-text-item icon variant="indeterminate">This is indeterminate helper text</pf-helper-text-item>
+  <pf-helper-text-item icon variant="warning">This is warning helper text</pf-helper-text-item>
+  <pf-helper-text-item icon variant="success">This is success helper text</pf-helper-text-item>
+  <pf-helper-text-item icon variant="error">This is error helper text</pf-helper-text-item>
+</pf-helper-text>`},{default:e(()=>[t(s,null,{default:e(()=>[t(n,{icon:""},{default:e(()=>[i("This is default helper text")]),_:1}),t(n,{icon:"",variant:"indeterminate"},{default:e(()=>[i("This is indeterminate helper text")]),_:1}),t(n,{icon:"",variant:"warning"},{default:e(()=>[i("This is warning helper text")]),_:1}),t(n,{icon:"",variant:"success"},{default:e(()=>[i("This is success helper text")]),_:1}),t(n,{icon:"",variant:"error"},{default:e(()=>[i("This is error helper text")]),_:1})]),_:1})]),_:1}),t(l,{title:"Static with custom icons",source:`<pf-helper-text>
+  <pf-helper-text-item>
+    <template #icon>
+      <info-icon />
+    </template>
+    This is default helper text
+  </pf-helper-text-item>
+  <pf-helper-text-item variant="indeterminate">
+    <template #icon>
+      <question-icon />
+    </template>
+    This is indeterminate helper text
+  </pf-helper-text-item>
+  <pf-helper-text-item variant="warning">
+    <template #icon>
+      <exclamation-icon />
+    </template>
+    This is warning helper text
+  </pf-helper-text-item>
+  <pf-helper-text-item variant="success">
+    <template #icon>
+      <xmark-icon />
+    </template>
+    This is success helper text
+  </pf-helper-text-item>
+  <pf-helper-text-item variant="error">
+    <template #icon>
+      <xmark-icon />
+    </template>
+    This is error helper text
+  </pf-helper-text-item>
+</pf-helper-text>`},{default:e(()=>[t(s,null,{default:e(()=>[t(n,null,{icon:e(()=>[t(a(v))]),default:e(()=>[i(" This is default helper text ")]),_:1}),t(n,{variant:"indeterminate"},{icon:e(()=>[t(a(T))]),default:e(()=>[i(" This is indeterminate helper text ")]),_:1}),t(n,{variant:"warning"},{icon:e(()=>[t(a(y))]),default:e(()=>[i(" This is warning helper text ")]),_:1}),t(n,{variant:"success"},{icon:e(()=>[t(a(h))]),default:e(()=>[i(" This is success helper text ")]),_:1}),t(n,{variant:"error"},{icon:e(()=>[t(a(h))]),default:e(()=>[i(" This is error helper text ")]),_:1})]),_:1})]),_:1}),t(l,{title:"Dynamic list",source:`<pf-helper-text>
+  <pf-helper-text-item icon dynamic>This is default helper text</pf-helper-text-item>
+  <pf-helper-text-item icon dynamic variant="indeterminate">This is indeterminate helper text</pf-helper-text-item>
+  <pf-helper-text-item icon dynamic variant="warning">This is warning helper text</pf-helper-text-item>
+  <pf-helper-text-item icon dynamic variant="success">This is success helper text</pf-helper-text-item>
+  <pf-helper-text-item icon dynamic variant="error">This is error helper text</pf-helper-text-item>
+</pf-helper-text>`},{default:e(()=>[t(s,null,{default:e(()=>[t(n,{icon:"",dynamic:""},{default:e(()=>[i("This is default helper text")]),_:1}),t(n,{icon:"",dynamic:"",variant:"indeterminate"},{default:e(()=>[i("This is indeterminate helper text")]),_:1}),t(n,{icon:"",dynamic:"",variant:"warning"},{default:e(()=>[i("This is warning helper text")]),_:1}),t(n,{icon:"",dynamic:"",variant:"success"},{default:e(()=>[i("This is success helper text")]),_:1}),t(n,{icon:"",dynamic:"",variant:"error"},{default:e(()=>[i("This is error helper text")]),_:1})]),_:1})]),_:1})]),_:1})}}});export{C as default};
---
+++ assets/Modal.story-Bb86dlSu.js
@@ -0,0 +1,39 @@
+import{H as b}from"./help-icon-DNP3-qFK.js";import{f as y,r as p,o as _,c as M,w as e,a,b as n,t as f,u as q,d as w,e as r}from"./index-D1ygUcxc.js";const k=w("b",null,"modal",-1),V=y({__name:"Modal.story",setup(C){const s=p(!1),i=p(!1),d=p(!1),m=p(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+    est laborum.`);return(T,t)=>{const h=r("component-info"),l=r("pf-button"),u=r("pf-modal"),c=r("story-canvas"),v=r("pf-tooltip"),g=r("doc-page");return _(),M(g,{name:"Components/Modal.story.vue",title:"Modal"},{description:e(()=>[a("A "),k,a(" displays important information to a user without requiring them to navigate to a new page.")]),apidocs:e(()=>[n(h,{src:"packages/core/src/components/Modal/Modal.vue",doc:{name:"PfModal",exportName:"default",displayName:"Modal",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"open",description:"Flag to show the modal",required:!1,type:{name:"boolean"}},{name:"noClose",description:"Flag to remove the close button in the header area of the modal",required:!1,type:{name:"boolean"}},{name:"noBodyWrapper",description:"Flag indicating if modal content should be placed in a modal box body wrapper",required:!1,type:{name:"boolean"}},{name:"disableFocusTrap",description:"Flag to disable focus trap",required:!1,type:{name:"boolean"}},{name:"elementToFocus",description:`The element to focus when the modal opens. By default the first
+focusable element will receive focus.`,required:!1,type:{name:"union",elements:[{name:"HTMLElement"},{name:"SVGElement"},{name:"string"}]}},{name:"title",description:"Simple text content of the Modal Header, also used for aria-label on the body",required:!1,type:{name:"string"}},{name:"titleLabel",description:"Optional title label text for screen readers",required:!1,type:{name:"string"}},{name:"titleIconVariant",description:`Optional alert icon (or other) to show before the title of the Modal Header
+When the predefined alert types are used the default styling
+will be automatically applied`,required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"success"'},{name:'"danger"'},{name:'"warning"'},{name:'"info"'}]}},{name:"appendTo",required:!1,type:{name:"union",elements:[{name:"HTMLElement"},{name:"string"},{name:"TSParenthesizedType"}]}},{name:"ariaDescribedby",description:"Id of Modal Box description",required:!1,type:{name:"string"}},{name:"variant",description:"Variant of the modal",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"small"'},{name:'"medium"'},{name:'"large"'}]},defaultValue:{func:!1,value:"'default'"}},{name:"width",description:"Default width of the modal.",required:!1,type:{name:"union",elements:[{name:"number"},{name:"string"}]}},{name:"maxWidth",description:"Maximum width of the modal.",required:!1,type:{name:"union",elements:[{name:"number"},{name:"string"}]}},{name:"position",description:"Alternate position of the modal",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"top"'}]},defaultValue:{func:!1,value:"'default'"}},{name:"positionOffset",description:"Offset from alternate position. Can be any valid CSS length/percentage",required:!1,type:{name:"string"}},{name:"descriptorId",description:"Id of the ModalBoxBody",required:!1,type:{name:"string"}},{name:"component",required:!1,type:{name:"union",elements:[{name:"string"},{name:"Component"}]},defaultValue:{func:!1,value:"'div'"}}],events:[{name:"update:open",type:{names:["boolean"]}}],slots:[{name:"header"},{name:"title-icon"},{name:"description"},{name:"help"},{name:"default"},{name:"footer"}],sourceFiles:["../../packages/core/src/components/Modal/Modal.vue"]}}),n(h,{src:"packages/core/src/components/Modal/ModalHeader.vue",doc:{name:"PfModalHeader",exportName:"default",displayName:"ModalHeader",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}}],slots:[{name:"default"},{name:"help"}],sourceFiles:["../../packages/core/src/components/Modal/ModalHeader.vue"]}})]),default:e(()=>[n(c,{title:"Default",source:`<pf-button @click="open1 = !open1">Toggle Modal</pf-button>
+<pf-modal v-model:open="open1" title="Simple modal header">
+  {{ sample_content }}
+</pf-modal>`},{default:e(()=>[n(l,{onClick:t[0]||(t[0]=o=>s.value=!s.value)},{default:e(()=>[a("Toggle Modal")]),_:1}),n(u,{open:s.value,"onUpdate:open":t[1]||(t[1]=o=>s.value=o),title:"Simple modal header"},{default:e(()=>[a(f(m.value),1)]),_:1},8,["open"])]),_:1}),n(c,{title:"With description",source:`<pf-button @click="open2 = !open2">Toggle Modal</pf-button>
+<pf-modal v-model:open="open2" title="Simple modal header">
+  {{ sample_content }}
+  <template #description>
+    A description is used when you want to provide more info about the modal than the title is able to describe. The content in the description is static and will not scroll with the rest of the modal body.
+  </template>
+  <template #footer>
+    <pf-button variant="primary" @click="open2 = !open2">
+      Confirm
+    </pf-button>
+    <pf-button variant="link" @click="open2 = !open2">
+      Cancel
+    </pf-button>
+  </template>
+</pf-modal>`},{default:e(()=>[n(l,{onClick:t[2]||(t[2]=o=>i.value=!i.value)},{default:e(()=>[a("Toggle Modal")]),_:1}),n(u,{open:i.value,"onUpdate:open":t[5]||(t[5]=o=>i.value=o),title:"Simple modal header"},{description:e(()=>[a(" A description is used when you want to provide more info about the modal than the title is able to describe. The content in the description is static and will not scroll with the rest of the modal body. ")]),footer:e(()=>[n(l,{variant:"primary",onClick:t[3]||(t[3]=o=>i.value=!i.value)},{default:e(()=>[a(" Confirm ")]),_:1}),n(l,{variant:"link",onClick:t[4]||(t[4]=o=>i.value=!i.value)},{default:e(()=>[a(" Cancel ")]),_:1})]),default:e(()=>[a(f(m.value)+" ",1)]),_:1},8,["open"])]),_:1}),n(c,{title:"With help",source:`<pf-button @click="open3 = !open3">Toggle Modal</pf-button>
+<pf-modal v-model:open="open3" title="Simple modal header">
+  {{ sample_content }}
+  <template #help>
+    <pf-tooltip>
+      <pf-button variant="plain">
+        <help-icon />
+      </pf-button>
+      <template #content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla
+        turpis.
+      </template>
+    </pf-tooltip>
+  </template>
+</pf-modal>`},{default:e(()=>[n(l,{onClick:t[6]||(t[6]=o=>d.value=!d.value)},{default:e(()=>[a("Toggle Modal")]),_:1}),n(u,{open:d.value,"onUpdate:open":t[7]||(t[7]=o=>d.value=o),title:"Simple modal header"},{help:e(()=>[n(v,null,{content:e(()=>[a(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id feugiat augue, nec fringilla turpis. ")]),default:e(()=>[n(l,{variant:"plain"},{default:e(()=>[n(q(b))]),_:1})]),_:1})]),default:e(()=>[a(f(m.value)+" ",1)]),_:1},8,["open"])]),_:1})]),_:1})}}});export{V as default};
