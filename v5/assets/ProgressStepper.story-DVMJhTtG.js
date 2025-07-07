bc318f7791937202620c2bf6c9ce238e7c8f698b
mtorromeo <massimiliano.torromeo@gmail.com>
2025-07-07 20:10:29 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@c73bb55a6ca2386d7ed49006cbbfc2787ee3b5d4 🚀

---
+++ v5/assets/ProgressStepper.story-DVMJhTtG.js
@@ -0,0 +1,73 @@
+import{f as C,g as b,r as v,a as o,c as y,o as S,w as t,d as p,b as s,e as n,u as h}from"./index-t8o304Tr.js";const P={name:"InProgressIcon",height:1024,width:1024,svgPath:"M513.417211,16.013668 L513.417211,112.013668 C513.406007,120.539653 520.082422,127.576718 528.597211,128.013668 C732.697211,136.513668 896.147211,305.013668 896.147211,511.343668 C896.147211,723.013668 724.007211,895.163668 512.417211,895.163668 C437.186239,895.277345 363.602574,873.135095 300.927211,831.523668 C294.58293,827.2982 286.138663,828.135095 280.747211,833.523668 L211.807211,902.353668 C208.502935,905.676185 206.82123,910.280199 207.20607,914.950237 C207.590911,919.620275 210.00361,923.886884 213.807211,926.623668 C300.709573,989.398037 405.213535,1023.13146 512.417211,1023.01398 C794.537211,1023.01398 1023.91724,793.433668 1023.91724,511.413668 C1024.00721,235.103668 804.007211,9.22366802 529.897211,0.00645736761 C525.571205,-0.116171039 521.377128,1.51204372 518.271915,4.52681369 C515.166702,7.54158367 513.415263,11.6857144 513.417211,16.013668 M97.1272107,212.923668 C64.2190689,258.479443 39.0698895,309.162997 22.6972107,362.923668 C21.2705092,367.756559 22.2039184,372.979173 25.2163356,377.018685 C28.2287528,381.058197 32.9681443,383.442545 38.0072107,383.453683 L139.407211,383.453683 C145.970195,383.46249 151.873012,379.462528 154.297211,373.363668 C164.307669,347.593304 177.068112,322.977652 192.357211,299.943668 C196.579666,293.633658 195.741339,285.216847 190.357211,279.863668 L121.497211,211.013668 C118.186635,207.662531 113.567544,205.940538 108.871166,206.30669 C104.174788,206.672841 99.8783994,209.089927 97.1272107,212.913668 M112.907211,511.433663 L17.0072107,511.433663 C12.679257,511.433663 8.5351263,513.183159 5.52035633,516.288373 C2.50558635,519.393586 0.877371594,523.587662 1.00721065,527.913668 C4.13104542,629.408217 37.6085847,727.635936 97.1172107,809.913668 C99.8539943,813.717268 104.120603,816.129968 108.790642,816.514809 C113.46068,816.899649 118.064693,815.217944 121.387211,811.913668 L190.217211,743.073668 C195.605784,737.682216 196.442679,729.237949 192.217211,722.893668 C153.520825,664.580171 131.611534,596.745437 128.887211,526.813668 C128.553964,518.220138 121.487197,511.427209 112.887211,511.433663",yOffset:0,xOffset:0},T=C(P),x={name:"PendingIcon",height:1024,width:1024,svgPath:"M512.1,895.9 C300.3,895.9 128.1,723.8 128.1,511.9 C128.1,300.2 300.3,127.9 512.1,127.9 C723.8,127.9 896,300.2 896.1,511.9 C896.1,723.7 723.8,895.9 512.1,895.9 M512.1,0 C229.7,0 0,229.7 0,512 C0,794.3 229.7,1024 512.1,1024 C794.3,1024 1024,794.3 1024,512 C1024,229.7 794.3,0 512.1,0 M704,288 L704,272 C704,263.2 696.8,256 688,256 L336,256 C327.2,256 320,263.2 320,272 L320,288 C320,447 480,449 480,512 C480,575 320,575 320,736 L320,752 C320,760.8 327.2,768 336,768 L688,768 C696.8,768 704,760.8 704,752 L704,736 C704,576 544,577 544,512 C544,447 704,448 704,288 M603.9,704 L420.3,704 C417.3,704 418.2,695 419.4,686.8 C426.8,634.5 480.1,617.4 495.2,612.3 C512.3,606.6 512.3,606.6 528.9,612.3 C544,617.6 597.6,635.4 604.8,687 C605.9,695.2 606.9,704 603.9,704",yOffset:0,xOffset:0},F=C(x),A=b({__name:"ProgressStepper.story",setup(w){const d=v(!1),l=v(!1);return(I,e)=>{const m=o("component-info"),r=o("pf-progress-step"),i=o("pf-progress-stepper"),a=o("story-canvas"),c=o("pf-checkbox"),f=o("pf-button"),u=o("pf-popover"),_=o("doc-page");return S(),y(_,{name:"Components/ProgressStepper.story.vue",title:"ProgressStepper"},{description:t(()=>e[2]||(e[2]=[n("Progress steppers have default icons associated with the variant property, and may be overriden and customized using the icon slot.")])),apidocs:t(()=>[s(m,{src:"packages/core/src/components/ProgressStepper/ProgressStepper.vue",doc:{name:"PfProgressStepper",exportName:"default",displayName:"ProgressStepper",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"centerAligned",description:"Flag indicating the progress stepper should be centered.",required:!1,type:{name:"boolean"}},{name:"vertical",description:"Flag indicating the progress stepper has a vertical layout.",required:!1,type:{name:"boolean"}},{name:"compact",description:"Flag indicating the progress stepper should be rendered compactly.",required:!1,type:{name:"boolean"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/ProgressStepper/ProgressStepper.vue"]}}),s(m,{src:"packages/core/src/components/ProgressStepper/ProgressStep.vue",doc:{name:"PfProgressStep",exportName:"default",displayName:"ProgressStep",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"variant",description:"Variant of the progress step. Each variant has a default icon.",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"success"'},{name:'"info"'},{name:'"pending"'},{name:'"warning"'},{name:'"danger"'}]}},{name:"current",description:"Flag indicating the progress step is the current step.",required:!1,type:{name:"boolean"}},{name:"description",description:"Description text of a progress step.",required:!1,type:{name:"string"}}],slots:[{name:"icon"},{name:"default"},{name:"description"}],sourceFiles:["../../packages/core/src/components/ProgressStepper/ProgressStep.vue"]}})]),default:t(()=>[e[29]||(e[29]=p("div",{class:"markdown pf-v5-c-content"},[p("h2",{class:"pf-v5-c-title"},"Examples")],-1)),s(a,{title:"Basic",source:`<pf-progress-stepper>
+  <pf-progress-step variant="success">First step</pf-progress-step>
+  <pf-progress-step variant="info" current>Second step</pf-progress-step>
+  <pf-progress-step variant="pending">Third step</pf-progress-step>
+</pf-progress-stepper>`},{default:t(()=>[s(i,null,{default:t(()=>[s(r,{variant:"success"},{default:t(()=>e[3]||(e[3]=[n("First step")])),_:1,__:[3]}),s(r,{variant:"info",current:""},{default:t(()=>e[4]||(e[4]=[n("Second step")])),_:1,__:[4]}),s(r,{variant:"pending"},{default:t(()=>e[5]||(e[5]=[n("Third step")])),_:1,__:[5]})]),_:1})]),_:1}),s(a,{title:"With description",source:`<pf-progress-stepper>
+  <pf-progress-step variant="success" description="This is the first thing to happen">First step</pf-progress-step>
+  <pf-progress-step variant="info" description="This is the second thing to happen" current>Second step</pf-progress-step>
+  <pf-progress-step variant="pending" description="This is the last thing to happen">Third step</pf-progress-step>
+</pf-progress-stepper>`},{default:t(()=>[s(i,null,{default:t(()=>[s(r,{variant:"success",description:"This is the first thing to happen"},{default:t(()=>e[6]||(e[6]=[n("First step")])),_:1,__:[6]}),s(r,{variant:"info",description:"This is the second thing to happen",current:""},{default:t(()=>e[7]||(e[7]=[n("Second step")])),_:1,__:[7]}),s(r,{variant:"pending",description:"This is the last thing to happen"},{default:t(()=>e[8]||(e[8]=[n("Third step")])),_:1,__:[8]})]),_:1})]),_:1}),s(a,{title:"With alignment",source:`<pf-checkbox v-model="vertical" label="Vertical alignment" />
+<pf-checkbox v-model="centerAligned" label="Center alignment" />
+<br>
+
+<pf-progress-stepper :vertical="vertical" :center-aligned="centerAligned">
+  <pf-progress-step variant="success" description="This is the first thing to happen">First step</pf-progress-step>
+  <pf-progress-step variant="info" description="This is the second thing to happen" current>Second step</pf-progress-step>
+  <pf-progress-step variant="pending" description="This is the last thing to happen">Third step</pf-progress-step>
+</pf-progress-stepper>`},{default:t(()=>[s(c,{modelValue:d.value,"onUpdate:modelValue":e[0]||(e[0]=g=>d.value=g),label:"Vertical alignment"},null,8,["modelValue"]),s(c,{modelValue:l.value,"onUpdate:modelValue":e[1]||(e[1]=g=>l.value=g),label:"Center alignment"},null,8,["modelValue"]),e[12]||(e[12]=p("br",null,null,-1)),s(i,{vertical:d.value,"center-aligned":l.value},{default:t(()=>[s(r,{variant:"success",description:"This is the first thing to happen"},{default:t(()=>e[9]||(e[9]=[n("First step")])),_:1,__:[9]}),s(r,{variant:"info",description:"This is the second thing to happen",current:""},{default:t(()=>e[10]||(e[10]=[n("Second step")])),_:1,__:[10]}),s(r,{variant:"pending",description:"This is the last thing to happen"},{default:t(()=>e[11]||(e[11]=[n("Third step")])),_:1,__:[11]})]),_:1},8,["vertical","center-aligned"])]),_:1,__:[12]}),s(a,{title:"All icons and custom",source:`<pf-progress-stepper>
+  <pf-progress-step variant="success">success</pf-progress-step>
+  <pf-progress-step variant="info">info</pf-progress-step>
+  <pf-progress-step variant="warning">warning</pf-progress-step>
+  <pf-progress-step variant="danger">danger</pf-progress-step>
+  <pf-progress-step current>
+    <template #icon><in-progress-icon /></template>
+    custom 1
+  </pf-progress-step>
+  <pf-progress-step>
+    <template #icon><pending-icon /></template>
+    custom 2
+  </pf-progress-step>
+</pf-progress-stepper>`},{default:t(()=>[s(i,null,{default:t(()=>[s(r,{variant:"success"},{default:t(()=>e[13]||(e[13]=[n("success")])),_:1,__:[13]}),s(r,{variant:"info"},{default:t(()=>e[14]||(e[14]=[n("info")])),_:1,__:[14]}),s(r,{variant:"warning"},{default:t(()=>e[15]||(e[15]=[n("warning")])),_:1,__:[15]}),s(r,{variant:"danger"},{default:t(()=>e[16]||(e[16]=[n("danger")])),_:1,__:[16]}),s(r,{current:""},{icon:t(()=>[s(h(T))]),default:t(()=>[e[17]||(e[17]=n(" custom 1 "))]),_:1,__:[17]}),s(r,null,{icon:t(()=>[s(h(F))]),default:t(()=>[e[18]||(e[18]=n(" custom 2 "))]),_:1,__:[18]})]),_:1})]),_:1}),s(a,{title:"With help popover",source:`<pf-progress-stepper>
+  <pf-progress-step variant="success">
+    <pf-popover position="right">
+      <pf-button>First step</pf-button>
+
+      <template #header>
+        <div>First step popover</div>
+      </template>
+      <template #body>
+        <div>Additional info or help text content.</div>
+      </template>
+    </pf-popover>
+  </pf-progress-step>
+
+  <pf-progress-step variant="danger">
+    <pf-popover position="right">
+      <pf-button>Second step</pf-button>
+
+      <template #header>
+        <div>Second step popover</div>
+      </template>
+      <template #body>
+        <div>Additional info or help text content.</div>
+      </template>
+    </pf-popover>
+  </pf-progress-step>
+
+  <pf-progress-step variant="info" current>
+    <pf-popover position="right">
+      <pf-button>Third step</pf-button>
+
+      <template #header>
+        <div>Third step popover</div>
+      </template>
+      <template #body>
+        <div>Additional info or help text content.</div>
+      </template>
+    </pf-popover>
+  </pf-progress-step>
+
+  <pf-progress-step variant="pending">
+    Fourth step
+  </pf-progress-step>
+</pf-progress-stepper>`},{default:t(()=>[s(i,null,{default:t(()=>[s(r,{variant:"success"},{default:t(()=>[s(u,{position:"right"},{header:t(()=>e[20]||(e[20]=[p("div",null,"First step popover",-1)])),body:t(()=>e[21]||(e[21]=[p("div",null,"Additional info or help text content.",-1)])),default:t(()=>[s(f,null,{default:t(()=>e[19]||(e[19]=[n("First step")])),_:1,__:[19]})]),_:1})]),_:1}),s(r,{variant:"danger"},{default:t(()=>[s(u,{position:"right"},{header:t(()=>e[23]||(e[23]=[p("div",null,"Second step popover",-1)])),body:t(()=>e[24]||(e[24]=[p("div",null,"Additional info or help text content.",-1)])),default:t(()=>[s(f,null,{default:t(()=>e[22]||(e[22]=[n("Second step")])),_:1,__:[22]})]),_:1})]),_:1}),s(r,{variant:"info",current:""},{default:t(()=>[s(u,{position:"right"},{header:t(()=>e[26]||(e[26]=[p("div",null,"Third step popover",-1)])),body:t(()=>e[27]||(e[27]=[p("div",null,"Additional info or help text content.",-1)])),default:t(()=>[s(f,null,{default:t(()=>e[25]||(e[25]=[n("Third step")])),_:1,__:[25]})]),_:1})]),_:1}),s(r,{variant:"pending"},{default:t(()=>e[28]||(e[28]=[n(" Fourth step ")])),_:1,__:[28]})]),_:1})]),_:1})]),_:1,__:[29]})}}});export{A as default};
