491757c4d027aaba61d3d2d3a2915be58181853e
mtorromeo <massimiliano.torromeo@gmail.com>
2025-08-18 08:51:01 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@a3483f9909a37ba0f103f6d3a931c06038872973 🚀

---
+++ v5/assets/Banner.story-CyHm2t7R.js
@@ -0,0 +1,53 @@
+import{B as m}from"./bell-icon-LkX6t8Ce.js";import{C as b}from"./circle-info-icon-DJkCf2dq.js";import{C as x}from"./circle-exclamation-icon-BKu8IGWE.js";import{C as c,T as _}from"./triangle-exclamation-icon-DCDOWkbz.js";import{g as v,a as o,c as g,o as B,w as a,b as n,d as f,e as t,u as i}from"./index--PMAKewJ.js";const D=v({__name:"Banner.story",setup(y){return(G,e)=>{const p=o("component-info"),r=o("pf-banner"),u=o("story-canvas"),l=o("pf-flex-item"),s=o("pf-flex"),d=o("doc-page");return B(),g(d,{name:"Components/Banner.story.vue",title:"Banner"},{description:a(()=>e[0]||(e[0]=[t("A ",-1),f("b",null,"banner",-1),t(" is a 1-line, full color, full width container that can be used to communicate short snippets of information to users. Banners are un-intrusive and non-dismissible.",-1)])),apidocs:a(()=>[n(p,{src:"packages/core/src/components/Banner.vue",doc:{name:"PfBanner",exportName:"default",displayName:"Banner",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"sticky",description:"If set to true, the banner sticks to the top of its container",required:!1,type:{name:"boolean"}},{name:"screenReaderText",description:`Text announced by screen readers to indicate the type of banner. This prop should only
+be passed in when the banner conveys status/severity.`,required:!1,type:{name:"string"}},{name:"variant",description:"Variant styles for the banner.",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"blue"'},{name:'"red"'},{name:'"green"'},{name:'"gold"'}]},defaultValue:{func:!1,value:"'default'"}}],slots:[{name:"screen-reader-text"},{name:"default"}],sourceFiles:["../../packages/core/src/components/Banner.vue"]}})]),default:a(()=>[n(u,{title:"Basic",source:`<pf-banner>Default banner</pf-banner>
+<br>
+<pf-banner variant="blue">Blue banner</pf-banner>
+<br>
+<pf-banner variant="red">Red banner</pf-banner>
+<br>
+<pf-banner variant="green">Green banner</pf-banner>
+<br>
+<pf-banner variant="gold">Gold banner</pf-banner>`},{default:a(()=>[n(r,null,{default:a(()=>e[1]||(e[1]=[t("Default banner",-1)])),_:1,__:[1]}),e[6]||(e[6]=f("br",null,null,-1)),n(r,{variant:"blue"},{default:a(()=>e[2]||(e[2]=[t("Blue banner",-1)])),_:1,__:[2]}),e[7]||(e[7]=f("br",null,null,-1)),n(r,{variant:"red"},{default:a(()=>e[3]||(e[3]=[t("Red banner",-1)])),_:1,__:[3]}),e[8]||(e[8]=f("br",null,null,-1)),n(r,{variant:"green"},{default:a(()=>e[4]||(e[4]=[t("Green banner",-1)])),_:1,__:[4]}),e[9]||(e[9]=f("br",null,null,-1)),n(r,{variant:"gold"},{default:a(()=>e[5]||(e[5]=[t("Gold banner",-1)])),_:1,__:[5]})]),_:1,__:[6,7,8,9]}),n(u,{title:"Status",source:`<pf-banner screen-reader-text="Default banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <bell-icon />
+    </pf-flex-item>
+    <pf-flex-item>Default banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner variant="blue" screen-reader-text="Blue banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-info-icon />
+    </pf-flex-item>
+    <pf-flex-item>Blue banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner variant="red" screen-reader-text="Red banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-exclamation-icon />
+    </pf-flex-item>
+    <pf-flex-item>Red banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner variant="green" screen-reader-text="Green banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-check-icon />
+    </pf-flex-item>
+    <pf-flex-item>Green banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner variant="gold" screen-reader-text="Gold banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <triangle-exclamation-icon />
+    </pf-flex-item>
+    <pf-flex-item>Gold banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>`},{default:a(()=>[n(r,{"screen-reader-text":"Default banner"},{default:a(()=>[n(s,{"space-items":"sm"},{default:a(()=>[n(l,null,{default:a(()=>[n(i(m))]),_:1}),n(l,null,{default:a(()=>e[10]||(e[10]=[t("Default banner",-1)])),_:1,__:[10]})]),_:1})]),_:1}),e[15]||(e[15]=f("br",null,null,-1)),n(r,{variant:"blue","screen-reader-text":"Blue banner"},{default:a(()=>[n(s,{"space-items":"sm"},{default:a(()=>[n(l,null,{default:a(()=>[n(i(b))]),_:1}),n(l,null,{default:a(()=>e[11]||(e[11]=[t("Blue banner",-1)])),_:1,__:[11]})]),_:1})]),_:1}),e[16]||(e[16]=f("br",null,null,-1)),n(r,{variant:"red","screen-reader-text":"Red banner"},{default:a(()=>[n(s,{"space-items":"sm"},{default:a(()=>[n(l,null,{default:a(()=>[n(i(x))]),_:1}),n(l,null,{default:a(()=>e[12]||(e[12]=[t("Red banner",-1)])),_:1,__:[12]})]),_:1})]),_:1}),e[17]||(e[17]=f("br",null,null,-1)),n(r,{variant:"green","screen-reader-text":"Green banner"},{default:a(()=>[n(s,{"space-items":"sm"},{default:a(()=>[n(l,null,{default:a(()=>[n(i(c))]),_:1}),n(l,null,{default:a(()=>e[13]||(e[13]=[t("Green banner",-1)])),_:1,__:[13]})]),_:1})]),_:1}),e[18]||(e[18]=f("br",null,null,-1)),n(r,{variant:"gold","screen-reader-text":"Gold banner"},{default:a(()=>[n(s,{"space-items":"sm"},{default:a(()=>[n(l,null,{default:a(()=>[n(i(_))]),_:1}),n(l,null,{default:a(()=>e[14]||(e[14]=[t("Gold banner",-1)])),_:1,__:[14]})]),_:1})]),_:1})]),_:1,__:[15,16,17,18]})]),_:1})}}});export{D as default};
---
+++ v5/assets/Navigation.story-Q-PavN6g.js
@@ -0,0 +1,80 @@
+import{g as L,r as c,a as r,c as y,o as _,w as t,b as e,e as i,d as N}from"./index--PMAKewJ.js";const b=L({__name:"Navigation.story",setup(q){const n=c("link1"),p=c("dark"),d=c("default");function o(k,a,s){n.value=s}return(k,a)=>{const s=r("component-info"),l=r("pf-nav-item"),u=r("pf-nav-list"),m=r("pf-nav"),f=r("story-canvas"),v=r("pf-nav-group"),g=r("doc-page");return _(),y(g,{name:"Components/Navigation.story.vue",title:"Navigation"},{description:t(()=>a[0]||(a[0]=[i("A ",-1),N("b",null,"navigation",-1),i(" organizes an application's structure and content, making it easy to find information and accomplish tasks. Navigation communicates relationships, context, and actions a user can take within an application.",-1)])),apidocs:t(()=>[e(s,{src:"packages/core/src/components/Nav/Nav.vue",doc:{name:"PfNav",exportName:"default",displayName:"Nav",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"theme",required:!1,type:{name:"union",elements:[{name:'"dark"'},{name:'"light"'}]},defaultValue:{func:!1,value:"'dark'"}},{name:"variant",required:!1,type:{name:"union",elements:[{name:'"default"'},{name:'"horizontal"'},{name:'"tertiary"'},{name:'"horizontal-subnav"'},{name:'"subnav"'}]},defaultValue:{func:!1,value:"'default'"}},{name:"ariaLabel",required:!1,type:{name:"string"}}],events:[{name:"select",type:{names:["Event"]}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Nav/Nav.vue"]}}),e(s,{src:"packages/core/src/components/Nav/NavExpandable.vue",doc:{name:"PfNavExpandable",exportName:"default",displayName:"NavExpandable",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"title",required:!1,type:{name:"string"}},{name:"srText",required:!1,type:{name:"string"}},{name:"groupId",required:!1,type:{name:"union",elements:[{name:"string"},{name:"number"}]}},{name:"id",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"() => useId()"}},{name:"active",required:!1,type:{name:"boolean"}},{name:"managed",required:!1,type:{name:"boolean"}},{name:"expanded",required:!1,type:{name:"boolean"}}],events:[{name:"update:expanded",type:{names:["boolean"]}}],slots:[{name:"title"},{name:"default"}],sourceFiles:["../../packages/core/src/components/Nav/NavExpandable.vue"]}}),e(s,{src:"packages/core/src/components/Nav/NavGroup.vue",doc:{name:"PfNavGroup",exportName:"default",displayName:"NavGroup",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"title",required:!0,type:{name:"string"}},{name:"id",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"() => useId()"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Nav/NavGroup.vue"]}}),e(s,{src:"packages/core/src/components/Nav/NavItem.vue",doc:{name:"PfNavItem",exportName:"default",displayName:"NavItem",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"component",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"'li'"}},{name:"linkComponent",required:!1,type:{name:"string"}},{name:"groupId",required:!1,type:{name:"string"}},{name:"itemId",required:!1,type:{name:"string"}},{name:"active",required:!1,type:{name:"boolean"}},{name:"preventDefault",required:!1,type:{name:"boolean"}},{name:"itemClass",required:!1,type:{name:"union",elements:[{name:"string"},{name:"Array",elements:[{name:"any"}]},{name:"Record",elements:[{name:"string"},{name:"boolean"}]}]}},{name:"itemAttrs",required:!1,type:{name:"Record",elements:[{name:"string"},{name:"any"}]}},{name:"to",required:!1,type:{name:"RouteLocationRaw"}},{name:"tabindex",required:!1,type:{name:"union",elements:[{name:"string"},{name:"number"}]}}],events:[{name:"showflyout"},{name:"select",type:{names:["Event"]}}],slots:[{name:"default"},{name:"flyout"}],sourceFiles:["../../packages/core/src/components/Nav/NavItem.vue"]}}),e(s,{src:"packages/core/src/components/Nav/NavItemSeparator.vue",doc:{name:"PfNavItemSeparator",exportName:"default",displayName:"NavItemSeparator",description:"",tags:{},sourceFiles:["../../packages/core/src/components/Nav/NavItemSeparator.vue"]}}),e(s,{src:"packages/core/src/components/Nav/NavList.vue",doc:{name:"PfNavList",exportName:"default",displayName:"NavList",description:"",tags:{},props:[{name:"ouiaSafe",description:"Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.",required:!1,type:{name:"boolean"}},{name:"ouiaId",description:"Value to overwrite the randomly generated data-ouia-component-id.",required:!1,type:{name:"OuiaId"}},{name:"ariaLeftScroll",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"'Scroll left'"}},{name:"ariaRightScroll",required:!1,type:{name:"string"},defaultValue:{func:!1,value:"'Scroll right'"}}],slots:[{name:"default"}],sourceFiles:["../../packages/core/src/components/Nav/NavList.vue"]}})]),default:t(()=>[e(f,{title:"Default",dark:p.value!=="light"&&d.value!=="tertiary","max-width":"300px",source:`<pf-nav>
+  <pf-nav-list>
+    <pf-nav-item item-id="link1" :active="activeLink == 'link1'" @select="selectLink">
+      Link 1
+    </pf-nav-item>
+    <pf-nav-item item-id="link2" :active="activeLink == 'link2'" @select="selectLink">
+      Link 2
+    </pf-nav-item>
+    <pf-nav-item item-id="link3" :active="activeLink == 'link3'" @select="selectLink">
+      Link 3
+    </pf-nav-item>
+    <pf-nav-item item-id="link4" :active="activeLink == 'link4'" @select="selectLink">
+      Link 4
+    </pf-nav-item>
+    <pf-nav-item item-id="link5" :active="activeLink == 'link5'" @select="selectLink">
+      Link 5
+    </pf-nav-item>
+  </pf-nav-list>
+</pf-nav>`},{default:t(()=>[e(m,null,{default:t(()=>[e(u,null,{default:t(()=>[e(l,{"item-id":"link1",active:n.value=="link1",onSelect:o},{default:t(()=>a[1]||(a[1]=[i(" Link 1 ",-1)])),_:1,__:[1]},8,["active"]),e(l,{"item-id":"link2",active:n.value=="link2",onSelect:o},{default:t(()=>a[2]||(a[2]=[i(" Link 2 ",-1)])),_:1,__:[2]},8,["active"]),e(l,{"item-id":"link3",active:n.value=="link3",onSelect:o},{default:t(()=>a[3]||(a[3]=[i(" Link 3 ",-1)])),_:1,__:[3]},8,["active"]),e(l,{"item-id":"link4",active:n.value=="link4",onSelect:o},{default:t(()=>a[4]||(a[4]=[i(" Link 4 ",-1)])),_:1,__:[4]},8,["active"]),e(l,{"item-id":"link5",active:n.value=="link5",onSelect:o},{default:t(()=>a[5]||(a[5]=[i(" Link 5 ",-1)])),_:1,__:[5]},8,["active"])]),_:1})]),_:1})]),_:1},8,["dark"]),e(f,{title:"Grouped",dark:p.value!=="light"&&d.value!=="tertiary","max-width":"300px",source:`<pf-nav @select="selectLink">
+  <pf-nav-list>
+    <pf-nav-group title="Section title 1">
+      <pf-nav-item item-id="link1" :active="activeLink == 'link1'">
+        Link 1
+      </pf-nav-item>
+      <pf-nav-item item-id="link2" :active="activeLink == 'link2'">
+        Link 2
+      </pf-nav-item>
+      <pf-nav-item item-id="link3" :active="activeLink == 'link3'">
+        Link 3
+      </pf-nav-item>
+    </pf-nav-group>
+    <pf-nav-group title="Section title 2">
+      <pf-nav-item item-id="link4" :active="activeLink == 'link4'">
+        Link 4
+      </pf-nav-item>
+      <pf-nav-item item-id="link5" :active="activeLink == 'link5'">
+        Link 5
+      </pf-nav-item>
+      <pf-nav-item item-id="link6" :active="activeLink == 'link6'">
+        Link 6
+      </pf-nav-item>
+    </pf-nav-group>
+  </pf-nav-list>
+</pf-nav>`},{default:t(()=>[e(m,{onSelect:o},{default:t(()=>[e(u,null,{default:t(()=>[e(v,{title:"Section title 1"},{default:t(()=>[e(l,{"item-id":"link1",active:n.value=="link1"},{default:t(()=>a[6]||(a[6]=[i(" Link 1 ",-1)])),_:1,__:[6]},8,["active"]),e(l,{"item-id":"link2",active:n.value=="link2"},{default:t(()=>a[7]||(a[7]=[i(" Link 2 ",-1)])),_:1,__:[7]},8,["active"]),e(l,{"item-id":"link3",active:n.value=="link3"},{default:t(()=>a[8]||(a[8]=[i(" Link 3 ",-1)])),_:1,__:[8]},8,["active"])]),_:1}),e(v,{title:"Section title 2"},{default:t(()=>[e(l,{"item-id":"link4",active:n.value=="link4"},{default:t(()=>a[9]||(a[9]=[i(" Link 4 ",-1)])),_:1,__:[9]},8,["active"]),e(l,{"item-id":"link5",active:n.value=="link5"},{default:t(()=>a[10]||(a[10]=[i(" Link 5 ",-1)])),_:1,__:[10]},8,["active"]),e(l,{"item-id":"link6",active:n.value=="link6"},{default:t(()=>a[11]||(a[11]=[i(" Link 6 ",-1)])),_:1,__:[11]},8,["active"])]),_:1})]),_:1})]),_:1})]),_:1},8,["dark"]),e(f,{title:"Flyout",dark:p.value!=="light"&&d.value!=="tertiary","max-width":"300px",source:`<pf-nav @select="selectLink">
+  <pf-nav @select="selectLink">
+    <pf-nav-list>
+      <pf-nav-group title="Section title 1">
+        <pf-nav-item item-id="link1" :active="activeLink == 'link1'">
+          Link 1
+        </pf-nav-item>
+        <pf-nav-item item-id="link2" :active="activeLink == 'link2'">
+          Link 2
+        </pf-nav-item>
+        <pf-nav-item item-id="link3" :active="activeLink == 'link3'">
+          Link 3
+          <template #flyout>
+            <pf-nav variant="subnav">
+              <pf-nav-list>
+                <pf-nav-item item-id="link7" :active="activeLink == 'link7'">
+                  Link 7
+                </pf-nav-item>
+              </pf-nav-list>
+            </pf-nav>
+          </template>
+        </pf-nav-item>
+      </pf-nav-group>
+      <pf-nav-group title="Section title 2">
+        <pf-nav-item item-id="link4" :active="activeLink == 'link4'">
+          Link 4
+        </pf-nav-item>
+        <pf-nav-item item-id="link5" :active="activeLink == 'link5'">
+          Link 5
+        </pf-nav-item>
+        <pf-nav-item item-id="link6" :active="activeLink == 'link6'">
+          Link 6
+        </pf-nav-item>
+      </pf-nav-group>
+    </pf-nav-list>
+  </pf-nav>
+</pf-nav>`},{default:t(()=>[e(m,{onSelect:o},{default:t(()=>[e(m,{onSelect:o},{default:t(()=>[e(u,null,{default:t(()=>[e(v,{title:"Section title 1"},{default:t(()=>[e(l,{"item-id":"link1",active:n.value=="link1"},{default:t(()=>a[12]||(a[12]=[i(" Link 1 ",-1)])),_:1,__:[12]},8,["active"]),e(l,{"item-id":"link2",active:n.value=="link2"},{default:t(()=>a[13]||(a[13]=[i(" Link 2 ",-1)])),_:1,__:[13]},8,["active"]),e(l,{"item-id":"link3",active:n.value=="link3"},{flyout:t(()=>[e(m,{variant:"subnav"},{default:t(()=>[e(u,null,{default:t(()=>[e(l,{"item-id":"link7",active:n.value=="link7"},{default:t(()=>a[14]||(a[14]=[i(" Link 7 ",-1)])),_:1,__:[14]},8,["active"])]),_:1})]),_:1})]),default:t(()=>[a[15]||(a[15]=i(" Link 3 ",-1))]),_:1,__:[15]},8,["active"])]),_:1}),e(v,{title:"Section title 2"},{default:t(()=>[e(l,{"item-id":"link4",active:n.value=="link4"},{default:t(()=>a[16]||(a[16]=[i(" Link 4 ",-1)])),_:1,__:[16]},8,["active"]),e(l,{"item-id":"link5",active:n.value=="link5"},{default:t(()=>a[17]||(a[17]=[i(" Link 5 ",-1)])),_:1,__:[17]},8,["active"]),e(l,{"item-id":"link6",active:n.value=="link6"},{default:t(()=>a[18]||(a[18]=[i(" Link 6 ",-1)])),_:1,__:[18]},8,["active"])]),_:1})]),_:1})]),_:1})]),_:1})]),_:1},8,["dark"])]),_:1})}}});export{b as default};
