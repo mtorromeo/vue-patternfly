f90225ff5cf5a3d48c0f1cb6756ff3cc4c784e6c
wojtask9 <wojtask9.kde@gmail.com>
2026-01-16 16:09:04 +0100

    fix(Toolbar): wrong breakpoint calculation

--- packages/core/src/components/Toolbar/common.ts
+++ packages/core/src/components/Toolbar/common.ts
@@ -4,8 +4,8 @@ import globalBreakpointXl from '@patternfly/react-tokens/dist/js/t_global_breakp
 import globalBreakpoint2xl from '@patternfly/react-tokens/dist/js/t_global_breakpoint_2xl';
 
 export const globalBreakpoints = {
-  md: parseInt(globalBreakpointMd.value),
-  lg: parseInt(globalBreakpointLg.value),
-  xl: parseInt(globalBreakpointXl.value),
-  '2xl': parseInt(globalBreakpoint2xl.value),
+  md: parseInt(globalBreakpointMd.value) * 16,
+  lg: parseInt(globalBreakpointLg.value) * 16,
+  xl: parseInt(globalBreakpointXl.value) * 16,
+  '2xl': parseInt(globalBreakpoint2xl.value) * 16,
 };
