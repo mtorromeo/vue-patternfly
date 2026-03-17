e41fbed273e5961a2bffc59284f3fcd5f4efac4f
mtorromeo <massimiliano.torromeo@gmail.com>
2026-03-17 18:08:00 +0000

    Deploying to gh-pages from @ mtorromeo/vue-patternfly@bc0586f363eae2a51cc523e58c0ab6d0ddc30023 🚀

---
+++ assets/Banner.story-Jgzx1fe5.js
@@ -0,0 +1,61 @@
+import{H as e,N as t,c as n,h as r,it as i,k as a,m as o,p as s,s as c}from"./runtime-core.esm-bundler-BYNplpMz.js";import{g as l,h as u,m as d,p as f}from"./index-DA-Rfo6V.js";var p=r({__name:`Banner.story`,setup(r){return(r,p)=>{let m=t(`component-info`),h=t(`pf-banner`),g=t(`story-canvas`),_=t(`pf-flex-item`),v=t(`pf-flex`),y=t(`doc-page`);return a(),n(y,{name:`Components/Banner.story.vue`,title:`Banner`},{description:e(()=>[...p[0]||=[s(`A `,-1),c(`b`,null,`banner`,-1),s(` is a 1-line, full color, full width container that can be used to communicate short snippets of information to users. Banners are un-intrusive and non-dismissible.`,-1)]]),apidocs:e(()=>[o(m,{name:`PfBanner`,doc:{name:`PfBanner`,exportName:`PfBanner`,displayName:`Banner`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}},{name:`sticky`,description:`If set to true, the banner sticks to the top of its container`,required:!1,type:{name:`boolean`}},{name:`screenReaderText`,description:`Text announced by screen readers to indicate the type of banner. This prop should only
+be passed in when the banner conveys status/severity.`,required:!1,type:{name:`string`}},{name:`color`,description:`Color options for the banner, will be overwritten by any applied using the status prop.`,required:!1,type:{name:`union`,elements:[{name:`"red"`},{name:`"orangered"`},{name:`"orange"`},{name:`"yellow"`},{name:`"green"`},{name:`"teal"`},{name:`"blue"`},{name:`"purple"`}]}},{name:`status`,description:`Status style options for the banner, will overwrite any color applied using the color prop.`,required:!1,type:{name:`union`,elements:[{name:`"success"`},{name:`"warning"`},{name:`"danger"`},{name:`"info"`},{name:`"custom"`}]}}],events:[],slots:[{name:`screen-reader-text`},{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/Banner.vue`]}})]),default:e(()=>[o(g,{title:`Basic`,source:`<pf-banner>Default banner</pf-banner>
+<br>
+<pf-banner color="red">Red banner</pf-banner>
+<br>
+<pf-banner color="orangered">Orangered banner</pf-banner>
+<br>
+<pf-banner color="orange">Orange banner</pf-banner>
+<br>
+<pf-banner color="yellow">Yellow banner</pf-banner>
+<br>
+<pf-banner color="green">Green banner</pf-banner>
+<br>
+<pf-banner color="teal">Teal banner</pf-banner>
+<br>
+<pf-banner color="blue">Blue banner</pf-banner>
+<br>
+<pf-banner color="purple">Purple banner</pf-banner>`},{default:e(()=>[o(h,null,{default:e(()=>[...p[1]||=[s(`Default banner`,-1)]]),_:1}),p[10]||=c(`br`,null,null,-1),o(h,{color:`red`},{default:e(()=>[...p[2]||=[s(`Red banner`,-1)]]),_:1}),p[11]||=c(`br`,null,null,-1),o(h,{color:`orangered`},{default:e(()=>[...p[3]||=[s(`Orangered banner`,-1)]]),_:1}),p[12]||=c(`br`,null,null,-1),o(h,{color:`orange`},{default:e(()=>[...p[4]||=[s(`Orange banner`,-1)]]),_:1}),p[13]||=c(`br`,null,null,-1),o(h,{color:`yellow`},{default:e(()=>[...p[5]||=[s(`Yellow banner`,-1)]]),_:1}),p[14]||=c(`br`,null,null,-1),o(h,{color:`green`},{default:e(()=>[...p[6]||=[s(`Green banner`,-1)]]),_:1}),p[15]||=c(`br`,null,null,-1),o(h,{color:`teal`},{default:e(()=>[...p[7]||=[s(`Teal banner`,-1)]]),_:1}),p[16]||=c(`br`,null,null,-1),o(h,{color:`blue`},{default:e(()=>[...p[8]||=[s(`Blue banner`,-1)]]),_:1}),p[17]||=c(`br`,null,null,-1),o(h,{color:`purple`},{default:e(()=>[...p[9]||=[s(`Purple banner`,-1)]]),_:1})]),_:1}),o(g,{title:`Status`,source:`<pf-banner status="success" screen-reader-text="Success banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-check-icon />
+    </pf-flex-item>
+    <pf-flex-item>Success banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner status="warning" screen-reader-text="Warning banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <triangle-exclamation-icon />
+    </pf-flex-item>
+    <pf-flex-item>Warning banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner status="danger" screen-reader-text="Danger banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-exclamation-icon />
+    </pf-flex-item>
+    <pf-flex-item>Danger banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner status="info" screen-reader-text="Info banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <circle-info-icon />
+    </pf-flex-item>
+    <pf-flex-item>Info banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>
+<br>
+<pf-banner status="custom" screen-reader-text="Custom banner">
+  <pf-flex space-items="sm">
+    <pf-flex-item>
+      <triangle-exclamation-icon />
+    </pf-flex-item>
+    <pf-flex-item>Custom banner</pf-flex-item>
+  </pf-flex>
+</pf-banner>`},{default:e(()=>[o(h,{status:`success`,"screen-reader-text":`Success banner`},{default:e(()=>[o(v,{"space-items":`sm`},{default:e(()=>[o(_,null,{default:e(()=>[o(i(l))]),_:1}),o(_,null,{default:e(()=>[...p[18]||=[s(`Success banner`,-1)]]),_:1})]),_:1})]),_:1}),p[23]||=c(`br`,null,null,-1),o(h,{status:`warning`,"screen-reader-text":`Warning banner`},{default:e(()=>[o(v,{"space-items":`sm`},{default:e(()=>[o(_,null,{default:e(()=>[o(i(d))]),_:1}),o(_,null,{default:e(()=>[...p[19]||=[s(`Warning banner`,-1)]]),_:1})]),_:1})]),_:1}),p[24]||=c(`br`,null,null,-1),o(h,{status:`danger`,"screen-reader-text":`Danger banner`},{default:e(()=>[o(v,{"space-items":`sm`},{default:e(()=>[o(_,null,{default:e(()=>[o(i(u))]),_:1}),o(_,null,{default:e(()=>[...p[20]||=[s(`Danger banner`,-1)]]),_:1})]),_:1})]),_:1}),p[25]||=c(`br`,null,null,-1),o(h,{status:`info`,"screen-reader-text":`Info banner`},{default:e(()=>[o(v,{"space-items":`sm`},{default:e(()=>[o(_,null,{default:e(()=>[o(i(f))]),_:1}),o(_,null,{default:e(()=>[...p[21]||=[s(`Info banner`,-1)]]),_:1})]),_:1})]),_:1}),p[26]||=c(`br`,null,null,-1),o(h,{status:`custom`,"screen-reader-text":`Custom banner`},{default:e(()=>[o(v,{"space-items":`sm`},{default:e(()=>[o(_,null,{default:e(()=>[o(i(d))]),_:1}),o(_,null,{default:e(()=>[...p[22]||=[s(`Custom banner`,-1)]]),_:1})]),_:1})]),_:1})]),_:1})]),_:1})}}});export{p as default};
---
+++ assets/EmptyState.story-CuXa22fO.js
@@ -0,0 +1,56 @@
+import{H as e,N as t,c as n,h as r,it as i,k as a,m as o,p as s,s as c}from"./runtime-core.esm-bundler-BYNplpMz.js";import{c as l,o as u}from"./index-DA-Rfo6V.js";import{t as d}from"./cubes-icon-DFtUTw_e.js";var f=r({__name:`EmptyState.story`,setup(r){return(r,f)=>{let p=t(`component-info`),m=t(`pf-empty-state-header`),h=t(`pf-empty-state-body`),g=t(`pf-button`),_=t(`pf-empty-state-actions`),v=t(`pf-empty-state-footer`),y=t(`pf-empty-state`),b=t(`story-canvas`),x=t(`pf-spinner`),S=t(`pf-empty-state-icon`),C=t(`doc-page`);return a(),n(C,{name:`Components/EmptyState.story.vue`,title:`Empty State`},{description:e(()=>[...f[0]||=[s(`An `,-1),c(`b`,null,`empty state`,-1),s(` is a screen that is not yet populated with data or information. Empty states typically contain a short message and next steps for the user.`,-1)]]),apidocs:e(()=>[o(p,{name:`PfEmptyState`,doc:{name:`PfEmptyState`,exportName:`PfEmptyState`,displayName:`EmptyState`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}},{name:`fullHeight`,description:`Cause component to consume the available height of its container`,required:!1,type:{name:`boolean`}},{name:`variant`,description:`Modifies EmptyState max-width`,required:!1,type:{name:`union`,elements:[{name:`"xs"`},{name:`"small"`},{name:`"large"`},{name:`"xl"`},{name:`"full"`}]}}],events:[],slots:[{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/EmptyState/EmptyState.vue`]}}),o(p,{name:`PfEmptyStateActions`,doc:{name:`PfEmptyStateActions`,exportName:`PfEmptyStateActions`,displayName:`EmptyStateActions`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}}],events:[],slots:[{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/EmptyState/EmptyStateActions.vue`]}}),o(p,{name:`PfEmptyStateBody`,doc:{name:`PfEmptyStateBody`,exportName:`PfEmptyStateHeader`,displayName:`EmptyStateHeader`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}},{name:`title`,description:`Text of the title inside empty state header, will be wrapped in headingLevel`,required:!1,type:{name:`string`}},{name:`headingLevel`,description:`The heading level to use, default is h1`,required:!1,type:{name:`union`,elements:[{name:`"h1"`},{name:`"h2"`},{name:`"h3"`},{name:`"h4"`},{name:`"h5"`},{name:`"h6"`}]},defaultValue:{func:!1,value:`'h1'`}}],events:[],slots:[{name:`icon`},{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/EmptyState/EmptyStateHeader.vue`]}}),o(p,{name:`PfEmptyStateFooter`,doc:{name:`PfEmptyStateFooter`,exportName:`PfEmptyStateFooter`,displayName:`EmptyStateFooter`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}}],events:[],slots:[{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/EmptyState/EmptyStateFooter.vue`]}}),o(p,{name:`PfEmptyStateHeader`}),o(p,{name:`PfEmptyStateIcon`,doc:{name:`PfEmptyStateIcon`,exportName:`PfEmptyStateIcon`,displayName:`EmptyStateIcon`,description:``,tags:{},props:[{name:`ouiaSafe`,description:`Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false.`,required:!1,type:{name:`boolean`}},{name:`ouiaId`,description:`Value to overwrite the randomly generated data-ouia-component-id.`,required:!1,type:{name:`OuiaId`}},{name:`color`,description:`Changes the color of the icon.`,required:!1,type:{name:`string`}}],events:[],slots:[{name:`default`}],sourceFiles:[`runner/work/vue-patternfly/vue-patternfly/packages/core/src/components/EmptyState/EmptyStateIcon.vue`]}})]),default:e(()=>[o(b,{title:`Example`,source:`<pf-empty-state>
+  <pf-empty-state-header title="Empty state">
+    <template #icon>
+      <cubes-icon />
+    </template>
+  </pf-empty-state-header>
+  <pf-empty-state-body>
+    This represents an the empty state pattern in Patternfly 6. Hopefully it's simple enough to use but flexible enough to meet a variety of needs.
+  </pf-empty-state-body>
+  <pf-empty-state-footer>
+    <pf-empty-state-actions>
+      <pf-button variant="primary">Primary action</pf-button>
+    </pf-empty-state-actions>
+    <pf-empty-state-actions>
+      <pf-button variant="link">Multiple</pf-button>
+      <pf-button variant="link">Action Buttons</pf-button>
+      <pf-button variant="link">Can</pf-button>
+      <pf-button variant="link">Go here</pf-button>
+      <pf-button variant="link">In the secondary</pf-button>
+      <pf-button variant="link">Action area</pf-button>
+    </pf-empty-state-actions>
+  </pf-empty-state-footer>
+</pf-empty-state>`},{default:e(()=>[o(y,null,{default:e(()=>[o(m,{title:`Empty state`},{icon:e(()=>[o(i(d))]),_:1}),o(h,null,{default:e(()=>[...f[1]||=[s(` This represents an the empty state pattern in Patternfly 6. Hopefully it's simple enough to use but flexible enough to meet a variety of needs. `,-1)]]),_:1}),o(v,null,{default:e(()=>[o(_,null,{default:e(()=>[o(g,{variant:`primary`},{default:e(()=>[...f[2]||=[s(`Primary action`,-1)]]),_:1})]),_:1}),o(_,null,{default:e(()=>[o(g,{variant:`link`},{default:e(()=>[...f[3]||=[s(`Multiple`,-1)]]),_:1}),o(g,{variant:`link`},{default:e(()=>[...f[4]||=[s(`Action Buttons`,-1)]]),_:1}),o(g,{variant:`link`},{default:e(()=>[...f[5]||=[s(`Can`,-1)]]),_:1}),o(g,{variant:`link`},{default:e(()=>[...f[6]||=[s(`Go here`,-1)]]),_:1}),o(g,{variant:`link`},{default:e(()=>[...f[7]||=[s(`In the secondary`,-1)]]),_:1}),o(g,{variant:`link`},{default:e(()=>[...f[8]||=[s(`Action area`,-1)]]),_:1})]),_:1})]),_:1})]),_:1})]),_:1}),o(b,{title:`Spinner`,source:`<pf-empty-state>
+  <pf-empty-state-header title="Loading" heading-level="h4">
+    <template #icon>
+      <pf-empty-state-icon>
+        <pf-spinner />
+      </pf-empty-state-icon>
+    </template>
+  </pf-empty-state-header>
+</pf-empty-state>`},{default:e(()=>[o(y,null,{default:e(()=>[o(m,{title:`Loading`,"heading-level":`h4`},{icon:e(()=>[o(S,null,{default:e(()=>[o(x)]),_:1})]),_:1})]),_:1})]),_:1}),o(b,{title:`No match found`,source:`<pf-empty-state>
+  <pf-empty-state-header title="No results found" heading-level="h4">
+    <template #icon>
+      <magnifying-glass-icon />
+    </template>
+  </pf-empty-state-header>
+  <pf-empty-state-body>
+    No results match the filter criteria. Clear all filters and try again.
+  </pf-empty-state-body>
+  <pf-empty-state-footer>
+    <pf-empty-state-actions>
+      <pf-button variant="link">Clear all filters</pf-button>
+    </pf-empty-state-actions>
+  </pf-empty-state-footer>
+</pf-empty-state>`},{default:e(()=>[o(y,null,{default:e(()=>[o(m,{title:`No results found`,"heading-level":`h4`},{icon:e(()=>[o(i(u))]),_:1}),o(h,null,{default:e(()=>[...f[9]||=[s(` No results match the filter criteria. Clear all filters and try again. `,-1)]]),_:1}),o(v,null,{default:e(()=>[o(_,null,{default:e(()=>[o(g,{variant:`link`},{default:e(()=>[...f[10]||=[s(`Clear all filters`,-1)]]),_:1})]),_:1})]),_:1})]),_:1})]),_:1}),o(b,{title:`Custom icon color`,source:`<pf-empty-state>
+  <pf-empty-state-header title="Custom icon color" heading-level="h4">
+    <template #icon>
+      <pf-empty-state-icon color="var(--pf-v6-global--success-color--100)">
+        <check-icon />
+      </pf-empty-state-icon>
+    </template>
+  </pf-empty-state-header>
+  <pf-empty-state-body>
+    This represents the empty state pattern in PatternFly with a custom icon color. Hopefully it's simple enough to use but flexible enough to meet a variety of needs
+  </pf-empty-state-body>
+</pf-empty-state>`},{default:e(()=>[o(y,null,{default:e(()=>[o(m,{title:`Custom icon color`,"heading-level":`h4`},{icon:e(()=>[o(S,{color:`var(--pf-v6-global--success-color--100)`},{default:e(()=>[o(i(l))]),_:1})]),_:1}),o(h,null,{default:e(()=>[...f[11]||=[s(` This represents the empty state pattern in PatternFly with a custom icon color. Hopefully it's simple enough to use but flexible enough to meet a variety of needs `,-1)]]),_:1})]),_:1})]),_:1})]),_:1})}}});export{f as default};
