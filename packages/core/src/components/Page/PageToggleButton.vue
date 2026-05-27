2c84ea977c66f506374c03a9094ca63c3870aae0
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2026-05-27 18:08:05 +0200

    fix(Page): pass sidebar state to button for expanded toggle state

--- packages/core/src/components/Page/PageToggleButton.vue
+++ packages/core/src/components/Page/PageToggleButton.vue
@@ -1,45 +1,40 @@
 <template>
-  <pf-button
-    v-bind="ouiaProps"
-    :id="id"
-    variant="plain"
-    aria-label="Side navigation toggle"
-    :aria-expanded="sidebarOpen ? 'true' : 'false'"
-    @click="sidebarOpen = !sidebarOpen"
-  >
+  <pf-button v-bind="ouiaProps" :id="id" variant="plain" :hamburger="hamburger" :expanded="hamburger && sidebarOpen" aria-label="Side navigation toggle" :aria-expanded="sidebarOpen ? 'true' : 'false'" @click="sidebarOpen = !sidebarOpen">
     <slot />
   </pf-button>
 </template>
 
 <script lang="ts">
-export const SidebarOpenKey = Symbol('SidebarOpenKey') as InjectionKey<ComputedRef<boolean> | boolean>;
+export const SidebarOpenKey = Symbol("SidebarOpenKey") as InjectionKey<ComputedRef<boolean> | boolean>;
 
-interface Props extends OUIAProps, /* @vue-ignore */ Omit<ComponentProps<typeof PfButton>, 'variant' | 'aria-label' | 'aria-expanded' | 'onClick' | 'ouiaId' | 'ouiaSafe'> {
+interface Props extends OUIAProps, /* @vue-ignore */ Omit<ComponentProps<typeof PfButton>, "variant" | "expanded" | "aria-label" | "aria-expanded" | "onClick" | "ouiaId" | "ouiaSafe"> {
   /** Sidebar id */
   id?: string;
   /** Programmatically manage if the side nav is shown, if managedSidebar is set to true in the PfPage component, this prop is managed */
   sidebarOpen?: boolean;
+  /** Flag indicating the button is a hamburger button. This will override the icon property. */
+  hamburger?: boolean;
 }
 </script>
 
 <script lang="ts" setup>
-import { computed, type ComputedRef, inject, type InjectionKey } from 'vue';
-import { PageManagedSidebarKey, PageSidebarOpenKey } from './Page.vue';
-import PfButton from '../Button.vue';
-import { useOUIAProps, type OUIAProps } from '../../helpers/ouia';
-import type { ComponentProps } from 'vue-component-type-helpers';
+import { computed, type ComputedRef, inject, type InjectionKey } from "vue";
+import { PageManagedSidebarKey, PageSidebarOpenKey } from "./Page.vue";
+import PfButton from "../Button.vue";
+import { useOUIAProps, type OUIAProps } from "../../helpers/ouia";
+import type { ComponentProps } from "vue-component-type-helpers";
 
 defineOptions({
-  name: 'PfPageToggleButton',
+  name: "PfPageToggleButton",
 });
 
 const props = withDefaults(defineProps<Props>(), {
-  id: 'nav-toggle',
+  id: "nav-toggle",
 });
-const ouiaProps = useOUIAProps({id: props.ouiaId, safe: props.ouiaSafe});
+const ouiaProps = useOUIAProps({ id: props.ouiaId, safe: props.ouiaSafe });
 
 const emit = defineEmits<{
-  (name: 'update:sidebarOpen', value: boolean): void;
+  (name: "update:sidebarOpen", value: boolean): void;
 }>();
 
 defineSlots<{
@@ -55,7 +50,7 @@ const sidebarOpen = computed({
     if (managedSidebar?.value && managedSidebarOpen) {
       managedSidebarOpen.value = value;
     } else {
-      emit('update:sidebarOpen', value);
+      emit("update:sidebarOpen", value);
     }
   },
 });
